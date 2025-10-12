9c2588502625200f5f23db1ce248af1ccdc1f2a6
Cyril-44 <zsy163mail1@163.com>
2025-10-12 23:20:32 +0800

    Now C

--- !Codeforces/CF2160C.cpp
+++ !Codeforces/CF2160C.cpp
@@ -1,31 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int f(int x) {
+int f(int x, int b) {
     int y = 0;
-    for (int i = 0; i < 31; i++) {
-        if (!(x >> i)) break;
+    for (int i = 0; i < b; i++) {
         y = (y << 1) | (x >> i & 1);
     }
     return y;
 }
 int main() {
     cin.tie(nullptr) -> sync_with_stdio(false);
-    freopen("CF2160C.helper", "w", stderr);
-    set<int> st;
-    for (int i = 1; i <= 1000; i++)
-        st.insert(i ^ f(i));
-    for (int i : st) fprintf(stderr, "%x %s\n", i, f(f(i) ^ i) == 0 ? "Yes" : "No");
-    fclose(stderr);
     int T, n;
     cin >> T;
     while (T--) {
         cin >> n;
-        for (int x = 0; x <= 20; x++) {
-            if (n == (x ^ f(x))) {
-                cout << n << " Find " << x << endl;
-                break;
-            }
-        }
+        if (n == 0) {puts("YES"); continue;}
+        int bits = 32 - __builtin_clz(n);
+        int now = f(n, bits) ^ n;
+        puts(now == f(now, bits) ? "Yes" : "No");
     }
     return 0;
 }
