d57e88034d0c59e2e024f67d14d5e5c314d53a6a
Cyril-44 <zsy163mail1@163.com>
2025-11-17 21:32:12 +0800

    CF2166

---
+++ !Codeforces/CF2166E.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+// #define LUOGU
+#if defined(ONLINE_JUDGE) && !defined(LUOGU)
+# pragma GCC optimize(2, 3, "inline", "unroll-loops", "fast-math", "inline-small-functions", "no-stack-protector", "delete-null-pointer-checks")
+# pragma GCC target("tune=native")
+#endif
+#define Inline __attribute__((always_inline)) inline
+#define For(i, s, t) for (int i = (s); i <= (t); ++i)
+#define Forv(i, s, t, ...) for (int i = (s), __VA_ARGS__; i <= (t); ++i)
+#define roF(i, t, s) for (int i = (t); i >= (s); --i)
+#define roFv(i, t, s, ...) for (int i = (t), __VA_ARGS__; i >= (s); --i)
+#define Rep(c) for (int tempFor_count = c; tempFor_count; --tempFor_count)
+#define Repv(c, ...) for (int tempFor_count = c, __VA_ARGS__; tempFor_count; --tempFor_count)
+#define YES return cout << "Yes\n", void()
+#define NO return cout << "No\n", void()
+#define YESNO(j) cout << ((j) ? "Yes\n" : "No\n")
+using namespace std;using pii_t=pair<int,int>;using pll_t=pair<int64_t,int64_t>;using veci_t=vector<int>;using vecl_t=vector<int64_t>;Inline int Popcnt(int x){return __builtin_popcount((unsigned)x);}Inline int Popcnt(unsigned x){return __builtin_popcount(x);}Inline int Popcnt(int64_t x){return __builtin_popcountll((uint64_t)x);}Inline int Popcnt(uint64_t x){return __builtin_popcountll(x);}Inline int Log2(int x){return 31-__builtin_clz((unsigned)x|1);}Inline int Log2(unsigned x){return 31-__builtin_clz(x|1);}Inline int Log2(int64_t x){return 63-__builtin_clzll((uint64_t)x|1);}Inline int Log2(uint64_t x){return 63-__builtin_clzll(x|1);}
+
+#define MULTI_TEST_CASES
+
+constexpr int N = 500005;
+int a[N];
+inline void solveSingleTestCase() {
+    int n, q;
+    cin >> n >> q;
+    For (i, 1, n) cin >> a[i];
+    sort(a+1, a+1+n, greater<int>());
+    Repv (q, c) {
+        cin >> c;
+        priority_queue<int> q(a+1, a+1+min(35, n));
+        int now = 1;
+        long long ans = 0;
+        roF (k, 30, 0) {
+            if (c >> k & 1 ^ 1) continue;
+            int tp = q.top(); q.pop();
+            if ((1<<k) > tp) ans += (1<<k) - tp;
+            else q.push(tp - (1<<k));
+        }
+        cout << ans << '\n';
+    }
+}
+int main() {
+    cin.tie(nullptr) -> sync_with_stdio(false);
+    int testCases = 1;
+#ifdef MULTI_TEST_CASES
+    cin >> testCases;
+#endif
+    while (testCases--) solveSingleTestCase();
+    return 0;
+}
