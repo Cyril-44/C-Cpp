4e729ba41d08e0bf658067eec2d90d3936088df7
Cyril-44 <zsy163mail1@163.com>
2025-11-25 01:13:06 +0800

    P5391

--- !Luogu/P5391.cpp
+++ !Luogu/P5391.cpp
@@ -4,55 +4,57 @@
 constexpr int N = 20005;
 std::vector<int> g[N];
 int n, V;
-struct { int dfn, top, size, dep, hson, fa, x, y; } tr[N];
+struct { int size, hson, fa, x, y, ans; } tr[N];
+int match[N];
+int f[17][N];
 namespace HLD {
-    int timestamp = 0;
     void dfs1(int u) {
         tr[u].size = 1;
         int mxs=0, mxp=0;
         for (int v : g[u]) {
-            tr[v].fa = u;
-            tr[v].dep = tr[u].dep + 1;
             dfs1(v);
             tr[u].size += tr[v].size;
-            if (tr[v].size < mxs)
+            if (tr[v].size > mxs)
                 mxs = tr[v].size, mxp = v;
         }
         tr[u].hson = mxp;
     }
-    void dfs2(int u) {
-        tr[u].dfn = ++timestamp;
+    void dfs2(int u, int dep) {
+        for (int i = tr[u].x; i <= V; i++)
+            f[dep][i] = std::max(f[dep][i], f[dep][i - tr[u].x] + tr[u].y);
+        tr[u].ans = f[dep][V];
         if (tr[u].hson) {
-            tr[tr[u].hson].top = tr[u].top;
-            dfs2(tr[u].hson);
             for (int v : g[u]) {
                 if (v == tr[u].hson) continue;
-                tr[v].top = v;
-                dfs2(v);
+                memcpy(f[dep + 1], f[dep], sizeof(int) * N);
+                tr[v].ans = tr[u].ans;
+                dfs2(v, dep + 1);
             }
+            dfs2(tr[u].hson, dep);
         }
     }
-    inline void hld() {
+    inline void solve(int tot) {
         dfs1(1);
-        tr[1].top = 1;
-        dfs2(1);
+        dfs2(1, 0);
     }
-}
-inline void dfs(int u) {
-    
 }
 int main() {
     scanf("%d%d", &n, &V);
-    int now = 0; char ch;
+    int now = 1, tot = 1; char s[10];
     for (int i = 1, x, y; i <= n; i++) {
-        scanf(" %c%*s", &ch);
-        if (ch == 'a') {
+        scanf(" %s", s);
+        if (*s == 'a') {
             scanf("%d%d", &x, &y);
-            g[now].push_back(now + 1);
-            ++now;
-            tr[now].x = x, tr[now].y = y;
+            g[now].push_back(++tot);
+            tr[tot].fa = now;
+            tr[tot].x = x, tr[tot].y = y;
+            now = tot;
         }
+        else now = tr[now].fa;
+        match[i] = now;
     }
-    HLD::hld();
+    HLD::solve(tot);
+    for (int i = 1; i <= n; i++)
+        printf("%d\n", tr[match[i]].ans);
     return 0;
 }
