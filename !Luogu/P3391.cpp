663ead51f79cf47b9e5c5768be8376962f445e56
Cyril-44 <zsy163mail1@163.com>
2025-12-13 13:44:50 +0800

    nmsl

--- !Luogu/P3391.cpp
+++ !Luogu/P3391.cpp
@@ -40,33 +40,44 @@ inline void zig(Node* v) {
     pushdown(v);
     auto u = v->rs;
     pushdown(u);
+    auto anc = v->fa;
     u->fa = v->fa;
+    if (anc) (anc->ls == v ? anc->ls : anc->rs) = u;
     v->rs = u->ls;
     u->ls = v;
     pushup(v);
     pushup(u);
 }
 inline void zag(Node* u) {
-    auto v = u->rs;
+    pushdown(u);
+    auto v = u->ls;
+    pushdown(v);
+    auto anc = u->fa;
     v->fa = u->fa;
+    if (anc) (anc->ls == u ? anc->ls : anc->rs) = v;
     u->ls = v->rs;
     v->rs = u;
+    pushup(u);
+    pushup(v);
 }
 void rotate(Node* u) {
-    (u->fa->ls == u) ? zig(u->fa) : zag(u->fa);
+    (u->fa->ls == u) ? zag(u->fa) : zig(u->fa);
 }
 void splay(Node* u, Node* tgt = nullptr) { // 伸展 u 使 u->fa = tgt（默认旋到顶）
     while (u->fa != tgt) {
         // (O) ==l=> (P) ==r=> (U) 或 (O) ==r=> (P) ==l=> (u)：两次 rotate(u)
         // (O) ==l=> (P) ==l=> (U) 或 (O) ==r=> (P) ==r=> (u)：rotate(u->fa), rotate(u)
-        if (u->fa->fa != tgt)
+        if (u->fa->fa != tgt) {
+            printf("%d", (u->fa->ls == u) == (u->fa->fa->ls == u->fa));
             (u->fa->ls == u) == (u->fa->fa->ls == u->fa) ? rotate(u->fa) : rotate(u);
+        }
         rotate(u);
     }
     if (!tgt) root = u; // 旋到根了
 }
 inline void build(int n) {
     auto u = alloc();
+    u->size = 1;
     for (int i = 1; i <= n; i++) {
         u->ls = alloc();
         u->ls->fa = u;
@@ -77,11 +88,14 @@ inline void build(int n) {
     auto guard = alloc();
     guard->dat = 0x7fffffff;
     guard->fa = u;
+    guard->size = 1;
+    u->ls = guard;
     splay(guard); // 维护 size
 }
 inline Node* rank(int rk) {
+    ++rk;
     auto u = root;
-    while (u->size ^ 1) {
+    while (u->size > 1) {
         int lsz = u->ls ? u->ls->size : 0;
         int rsz = u->rs ? u->rs->size : 0;
         if (lsz < rk && rk <= u->size - rsz) break;
