2a6be68e0644e74132127a9ea8da64813800d83c
Cyril <Hardsoft.Cyril@outlook.com>
2025-12-16 21:04:25 +0800

    splay #2

--- Functions/Code Obfuscator/OBF.in.cpp
+++ Functions/Code Obfuscator/OBF.in.cpp
@@ -1,72 +1,95 @@
-#include <algorithm>
-#include <climits>
-#include <cstring>
-#include <iostream>
-#include <vector>
-
-using ll = long long;
-
-constexpr int maxn = 1e5 + 100;
-
-ll d[maxn];
-bool vis[maxn];
-ll ans;
-
-int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
-
-void upd(int step, int M) {
-  int D = gcd(step, M);
-  int len = M / D;
-
-  for (int st = 0; st < D; st++) {
-    if (vis[st]) continue;
-    std::vector<int> v;
-    int u = st;
-    for (int i = 0; i < len; i++) {
-      v.push_back(u);
-      vis[u] = true;
-      u = (u + step) % M;
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+template <class T> void in(T &x) {
+    x = 0;
+    int f = 1;
+    char c = getchar();
+    while (c < '0' || c > '9') {
+        if (c == '-') f = -1;
+        c = getchar();
     }
-    for (int r = 0; r < 2; r++) {
-      for (int i = 0; i < len; i++) {
-        int las = v[i];
-        int now = v[(i + 1) % len];
-        if (d[las] != LLONG_MAX) {
-          // 如果d[las]=LLONG_MAX，d[las]+step会越界，需要特判
-          d[now] = std::min(d[now], d[las] + step);
-        }
-      }
+    while (c >= '0' && c <= '9') {
+        x = x * 10 + c - '0';
+        c = getchar();
     }
-  }
 }
-
-int main() {
-  ll h;
-  std::cin >> h;
-  int x[3];
-  for (int i = 0; i < 3; i++) {
-    std::cin >> x[i];
-  }
-  std::sort(x, x + 3);
-  int M = x[0];
-
-  for (int i = 0; i < M; i++) {
-    d[i] = LLONG_MAX;
-    // 本题的h达到了ll的上界，如果使用ll的话必需把初值置为LLONG_MAX
-  }
-  d[0] = 0;
-
-  upd(x[1], M);
-  memset(vis, 0, sizeof(vis));
-  upd(x[2], M);
-
-  ll H = h - 1;
-  ans = 0;
-  for (int i = 0; i < M; i++) {
-    if (d[i] <= H && d[i] != LLONG_MAX) {
-      ans += (H - d[i]) / M + 1;
+const int N = 1000010, M = 20, P = 30;
+vector<int> gr[N];
+int n, m, a[N], fa, cur, d;
+struct Query {
+    int dep, id;
+};
+vector<Query> q[N];
+int dfn[N], son[N], dep[N], mxdep[N], ck;
+void init(int u) {
+    mxdep[u] = dep[u];
+    for (int v : gr[u]) {
+        dep[v] = dep[u] + 1;
+        init(v);
+        mxdep[u] = max(mxdep[u], mxdep[v]);
+        if (mxdep[son[u]] <= mxdep[v]) son[u] = v;
     }
-  }
-  printf("%lld\n", ans);
-  return 0;
+}
+ll f[N][M], g[N][P];
+int cnt[N];
+int c(int u, int len) {
+    if (mxdep[u] - dep[u] >= len) return dfn[u] + len;
+    return 0;
+}
+ll ans[N];
+void dfs(int u) {
+    dfn[u] = ++ck;
+    if (son[u]) dfs(son[u]);
+    for (int v : gr[u]) {
+        if (v == son[u]) continue;
+        dfs(v);
+        int add = dep[son[u]] - dep[v];
+        for (int i = 0; i <= mxdep[v] - dep[v]; i++) {
+            for (int j = 0; j < M; j++) f[c(son[u], i)][j] += f[c(v, i)][j];
+            for (int j = 0; j < P; j++) g[c(son[u], i)][j] += g[c(v, i)][j];
+            cnt[c(son[u], i)] += cnt[c(v, i)];
+        }
+    }
+    f[c(u, 0)][0] = 0;
+    for (int j = 0; j < P; j++) g[c(u, 0)][j] = g[c(u, 1)][j] + (a[u] & (1 << j));
+    cnt[c(u, 0)] = cnt[c(u, 1)] + 1;
+    for (int j = 1; j < M; j++) {
+        f[c(u, 0)][j] = f[c(u, 0)][j - 1] + g[c(u, 0)][j - 1] - g[c(u, 1 << j - 1)][j - 1];
+        f[c(u, 0)][j] += f[c(u, 1 << j - 1)][j - 1] + (1ll << j - 1) * (cnt[c(u, 1 << j - 1)] - cnt[c(u, 1 << j)]) -
+                         g[c(u, 1 << j - 1)][j - 1] + g[c(u, 1 << j)][j - 1];
+    }
+    for (const Query &x : q[u]) {
+        int pre = 0, lst = P - 1;
+        for (int i = M - 1; i >= 0; i--) {
+            if (x.dep & (1 << i)) {
+                ans[x.id] += f[c(u, pre)][i];
+                for (; lst > i; lst--) ans[x.id] += g[c(u, pre)][lst] - g[c(u, x.dep)][lst];
+                ans[x.id] += g[c(u, pre)][lst] - g[c(u, pre + (1 << lst))][lst];
+                ans[x.id] += (cnt[c(u, pre + (1 << lst))] - cnt[c(u, x.dep)]) * (1ll << lst);
+                ans[x.id] -= g[c(u, pre + (1 << lst))][lst] - g[c(u, x.dep)][lst];
+                lst--;
+                pre += (1 << i);
+            }
+        }
+    }
+}
+int main() {
+    in(n);
+    for (int i = 1; i <= n; i++) in(a[i]);
+    for (int i = 2; i <= n; i++) {
+        in(fa);
+        gr[fa].push_back(i);
+    }
+    in(m);
+    for (int i = 1; i <= m; i++) {
+        in(cur);
+        in(d);
+        d++;
+        q[cur].push_back((Query){d, i});
+    }
+    init(1);
+    dfs(1);
+    for (int i = 1; i <= m; i++) printf("%lld\n", ans[i]);
+    return 0;
 }
--- Functions/Code Obfuscator/OBF.in.cpp.obf.raw.cpp
+++ Functions/Code Obfuscator/OBF.in.cpp.obf.raw.cpp
@@ -1,72 +1,95 @@
-#include <algorithm>
-#include <climits>
-#include <cstring>
-#include <iostream>
-#include <vector>
-
-using JMGhI7Alck_ = long long;
-
-constexpr int zR8HHsX = 1e5 + 100;
-
-JMGhI7Alck_ YL48slIvOhH[zR8HHsX];
-bool tuorw8HHihO_[zR8HHsX];
-JMGhI7Alck_ t0DaqE;
-
-int wHY_J_Zn(int fwE2aA, int g11d9fWyt) { return g11d9fWyt ? wHY_J_Zn(g11d9fWyt, fwE2aA % g11d9fWyt) : fwE2aA; }
-
-void U5nKLZQEq6M(int lzw_fqjY, int ili_m8nWIqJ) {
-  int OdC8J6N = wHY_J_Zn(lzw_fqjY, ili_m8nWIqJ);
-  int zkVRp_X = ili_m8nWIqJ / OdC8J6N;
-
-  for (int BYXchFo4KCdu = 0; BYXchFo4KCdu < OdC8J6N; BYXchFo4KCdu++) {
-    if (tuorw8HHihO_[BYXchFo4KCdu]) continue;
-    std::vector<int> FMhsysIGqDp;
-    int SYaKKHA = BYXchFo4KCdu;
-    for (int OW4fST1qs = 0; OW4fST1qs < zkVRp_X; OW4fST1qs++) {
-      FMhsysIGqDp.push_back(SYaKKHA);
-      tuorw8HHihO_[SYaKKHA] = true;
-      SYaKKHA = (SYaKKHA + lzw_fqjY) % ili_m8nWIqJ;
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long zLt4deMAFHl5;
+template <class Izl76iItGZ> void QH9dl0mdIjTvA_yw(Izl76iItGZ &gTwC6OHdybL) {
+    gTwC6OHdybL = 0;
+    int R98bH8V6 = 1;
+    char VBEE7PC = getchar();
+    while (VBEE7PC < '0' || VBEE7PC > '9') {
+        if (VBEE7PC == '-') R98bH8V6 = -1;
+        VBEE7PC = getchar();
     }
-    for (int Kffyta1hV = 0; Kffyta1hV < 2; Kffyta1hV++) {
-      for (int X712P1kiW = 0; X712P1kiW < zkVRp_X; X712P1kiW++) {
-        int ikn8cRCtH_ = FMhsysIGqDp[X712P1kiW];
-        int ZaR6UVIqx = FMhsysIGqDp[(X712P1kiW + 1) % zkVRp_X];
-        if (YL48slIvOhH[ikn8cRCtH_] != LLONG_MAX) {
-          // 如果d[las]=LLONG_MAX，d[las]+step会越界，需要特判
-          YL48slIvOhH[ZaR6UVIqx] = std::min(YL48slIvOhH[ZaR6UVIqx], YL48slIvOhH[ikn8cRCtH_] + lzw_fqjY);
-        }
-      }
+    while (VBEE7PC >= '0' && VBEE7PC <= '9') {
+        gTwC6OHdybL = gTwC6OHdybL * 10 + VBEE7PC - '0';
+        VBEE7PC = getchar();
     }
-  }
 }
-
-int main() {
-  JMGhI7Alck_ RbkCczk4g;
-  std::cin >> RbkCczk4g;
-  int K_JJwFNeKf[3];
-  for (int kmKeesX = 0; kmKeesX < 3; kmKeesX++) {
-    std::cin >> K_JJwFNeKf[kmKeesX];
-  }
-  std::sort(K_JJwFNeKf, K_JJwFNeKf + 3);
-  int FuDuaqYeFE = K_JJwFNeKf[0];
-
-  for (int FnPIaQgqHk6F = 0; FnPIaQgqHk6F < FuDuaqYeFE; FnPIaQgqHk6F++) {
-    YL48slIvOhH[FnPIaQgqHk6F] = LLONG_MAX;
-    // 本题的h达到了ll的上界，如果使用ll的话必需把初值置为LLONG_MAX
-  }
-  YL48slIvOhH[0] = 0;
-
-  U5nKLZQEq6M(K_JJwFNeKf[1], FuDuaqYeFE);
-  memset(tuorw8HHihO_, 0, sizeof(tuorw8HHihO_));
-  U5nKLZQEq6M(K_JJwFNeKf[2], FuDuaqYeFE);
-
-  JMGhI7Alck_ JeGqFm = RbkCczk4g - 1;
-  t0DaqE = 0;
-  for (int vpkOi7 = 0; vpkOi7 < FuDuaqYeFE; vpkOi7++) {
-    if (YL48slIvOhH[vpkOi7] <= JeGqFm && YL48slIvOhH[vpkOi7] != LLONG_MAX) {
-      t0DaqE += (JeGqFm - YL48slIvOhH[vpkOi7]) / FuDuaqYeFE + 1;
+const int b5GXwXiW = 1000010, aUhJx3a9OK = 20, fUtwk7Y4eAp = 30;
+vector<int> xkkwOyR[b5GXwXiW];
+int mYYbAfGkOj, hbDL2G, glsRvEDLDlZt[b5GXwXiW], ugirUB2BUlJ, g1LnJmh, YSJUxckw;
+struct RQ7x6ieqUc {
+    int pyoLsQFQ, sqcBlIclA;
+};
+vector<RQ7x6ieqUc> uzmHlPQJe[b5GXwXiW];
+int wy3HMK[b5GXwXiW], oxYFgzp[b5GXwXiW], gOQ_bNyGa[b5GXwXiW], SD7ZEGex[b5GXwXiW], FH2THbuICt;
+void lFbYdl(int DtyN0Q8rB) {
+    SD7ZEGex[DtyN0Q8rB] = gOQ_bNyGa[DtyN0Q8rB];
+    for (int zWW0TjUfZHT : xkkwOyR[DtyN0Q8rB]) {
+        gOQ_bNyGa[zWW0TjUfZHT] = gOQ_bNyGa[DtyN0Q8rB] + 1;
+        lFbYdl(zWW0TjUfZHT);
+        SD7ZEGex[DtyN0Q8rB] = max(SD7ZEGex[DtyN0Q8rB], SD7ZEGex[zWW0TjUfZHT]);
+        if (SD7ZEGex[oxYFgzp[DtyN0Q8rB]] <= SD7ZEGex[zWW0TjUfZHT]) oxYFgzp[DtyN0Q8rB] = zWW0TjUfZHT;
     }
-  }
-  printf("%lld\n", t0DaqE);
-  return 0;
+}
+zLt4deMAFHl5 hqd_QJbg[b5GXwXiW][aUhJx3a9OK], su8v2rEAV67_[b5GXwXiW][fUtwk7Y4eAp];
+int E2NJhK18[b5GXwXiW];
+int ML_XQK(int bM3KMuv7, int WbjmPb8vl7M) {
+    if (SD7ZEGex[bM3KMuv7] - gOQ_bNyGa[bM3KMuv7] >= WbjmPb8vl7M) return wy3HMK[bM3KMuv7] + WbjmPb8vl7M;
+    return 0;
+}
+zLt4deMAFHl5 rGT0eQ[b5GXwXiW];
+void u6_UZSnQ(int UwKrjyIi) {
+    wy3HMK[UwKrjyIi] = ++FH2THbuICt;
+    if (oxYFgzp[UwKrjyIi]) u6_UZSnQ(oxYFgzp[UwKrjyIi]);
+    for (int nvF_Y78O3D : xkkwOyR[UwKrjyIi]) {
+        if (nvF_Y78O3D == oxYFgzp[UwKrjyIi]) continue;
+        u6_UZSnQ(nvF_Y78O3D);
+        int HnmPcAC4 = gOQ_bNyGa[oxYFgzp[UwKrjyIi]] - gOQ_bNyGa[nvF_Y78O3D];
+        for (int FgZQiJsptmNP = 0; FgZQiJsptmNP <= SD7ZEGex[nvF_Y78O3D] - gOQ_bNyGa[nvF_Y78O3D]; FgZQiJsptmNP++) {
+            for (int JSutlR6dEB = 0; JSutlR6dEB < aUhJx3a9OK; JSutlR6dEB++) hqd_QJbg[ML_XQK(oxYFgzp[UwKrjyIi], FgZQiJsptmNP)][JSutlR6dEB] += hqd_QJbg[ML_XQK(nvF_Y78O3D, FgZQiJsptmNP)][JSutlR6dEB];
+            for (int WwQatw = 0; WwQatw < fUtwk7Y4eAp; WwQatw++) su8v2rEAV67_[ML_XQK(oxYFgzp[UwKrjyIi], FgZQiJsptmNP)][WwQatw] += su8v2rEAV67_[ML_XQK(nvF_Y78O3D, FgZQiJsptmNP)][WwQatw];
+            E2NJhK18[ML_XQK(oxYFgzp[UwKrjyIi], FgZQiJsptmNP)] += E2NJhK18[ML_XQK(nvF_Y78O3D, FgZQiJsptmNP)];
+        }
+    }
+    hqd_QJbg[ML_XQK(UwKrjyIi, 0)][0] = 0;
+    for (int ptdvzm6W_CJ = 0; ptdvzm6W_CJ < fUtwk7Y4eAp; ptdvzm6W_CJ++) su8v2rEAV67_[ML_XQK(UwKrjyIi, 0)][ptdvzm6W_CJ] = su8v2rEAV67_[ML_XQK(UwKrjyIi, 1)][ptdvzm6W_CJ] + (glsRvEDLDlZt[UwKrjyIi] & (1 << ptdvzm6W_CJ));
+    E2NJhK18[ML_XQK(UwKrjyIi, 0)] = E2NJhK18[ML_XQK(UwKrjyIi, 1)] + 1;
+    for (int msTFtCjSOi = 1; msTFtCjSOi < aUhJx3a9OK; msTFtCjSOi++) {
+        hqd_QJbg[ML_XQK(UwKrjyIi, 0)][msTFtCjSOi] = hqd_QJbg[ML_XQK(UwKrjyIi, 0)][msTFtCjSOi - 1] + su8v2rEAV67_[ML_XQK(UwKrjyIi, 0)][msTFtCjSOi - 1] - su8v2rEAV67_[ML_XQK(UwKrjyIi, 1 << msTFtCjSOi - 1)][msTFtCjSOi - 1];
+        hqd_QJbg[ML_XQK(UwKrjyIi, 0)][msTFtCjSOi] += hqd_QJbg[ML_XQK(UwKrjyIi, 1 << msTFtCjSOi - 1)][msTFtCjSOi - 1] + (1ll << msTFtCjSOi - 1) * (E2NJhK18[ML_XQK(UwKrjyIi, 1 << msTFtCjSOi - 1)] - E2NJhK18[ML_XQK(UwKrjyIi, 1 << msTFtCjSOi)]) -
+                         su8v2rEAV67_[ML_XQK(UwKrjyIi, 1 << msTFtCjSOi - 1)][msTFtCjSOi - 1] + su8v2rEAV67_[ML_XQK(UwKrjyIi, 1 << msTFtCjSOi)][msTFtCjSOi - 1];
+    }
+    for (const RQ7x6ieqUc &IwJrbplS : uzmHlPQJe[UwKrjyIi]) {
+        int gFGEMSOTEwi = 0, LtYWpM66BXgE = fUtwk7Y4eAp - 1;
+        for (int dBIJdh = aUhJx3a9OK - 1; dBIJdh >= 0; dBIJdh--) {
+            if (IwJrbplS.pyoLsQFQ & (1 << dBIJdh)) {
+                rGT0eQ[IwJrbplS.sqcBlIclA] += hqd_QJbg[ML_XQK(UwKrjyIi, gFGEMSOTEwi)][dBIJdh];
+                for (; LtYWpM66BXgE > dBIJdh; LtYWpM66BXgE--) rGT0eQ[IwJrbplS.sqcBlIclA] += su8v2rEAV67_[ML_XQK(UwKrjyIi, gFGEMSOTEwi)][LtYWpM66BXgE] - su8v2rEAV67_[ML_XQK(UwKrjyIi, IwJrbplS.pyoLsQFQ)][LtYWpM66BXgE];
+                rGT0eQ[IwJrbplS.sqcBlIclA] += su8v2rEAV67_[ML_XQK(UwKrjyIi, gFGEMSOTEwi)][LtYWpM66BXgE] - su8v2rEAV67_[ML_XQK(UwKrjyIi, gFGEMSOTEwi + (1 << LtYWpM66BXgE))][LtYWpM66BXgE];
+                rGT0eQ[IwJrbplS.sqcBlIclA] += (E2NJhK18[ML_XQK(UwKrjyIi, gFGEMSOTEwi + (1 << LtYWpM66BXgE))] - E2NJhK18[ML_XQK(UwKrjyIi, IwJrbplS.pyoLsQFQ)]) * (1ll << LtYWpM66BXgE);
+                rGT0eQ[IwJrbplS.sqcBlIclA] -= su8v2rEAV67_[ML_XQK(UwKrjyIi, gFGEMSOTEwi + (1 << LtYWpM66BXgE))][LtYWpM66BXgE] - su8v2rEAV67_[ML_XQK(UwKrjyIi, IwJrbplS.pyoLsQFQ)][LtYWpM66BXgE];
+                LtYWpM66BXgE--;
+                gFGEMSOTEwi += (1 << dBIJdh);
+            }
+        }
+    }
+}
+int main() {
+    QH9dl0mdIjTvA_yw(mYYbAfGkOj);
+    for (int LcdggwY = 1; LcdggwY <= mYYbAfGkOj; LcdggwY++) QH9dl0mdIjTvA_yw(glsRvEDLDlZt[LcdggwY]);
+    for (int QpiQugrQrb = 2; QpiQugrQrb <= mYYbAfGkOj; QpiQugrQrb++) {
+        QH9dl0mdIjTvA_yw(ugirUB2BUlJ);
+        xkkwOyR[ugirUB2BUlJ].push_back(QpiQugrQrb);
+    }
+    QH9dl0mdIjTvA_yw(hbDL2G);
+    for (int L4t1rxnhRrF = 1; L4t1rxnhRrF <= hbDL2G; L4t1rxnhRrF++) {
+        QH9dl0mdIjTvA_yw(g1LnJmh);
+        QH9dl0mdIjTvA_yw(YSJUxckw);
+        YSJUxckw++;
+        uzmHlPQJe[g1LnJmh].push_back((RQ7x6ieqUc){YSJUxckw, L4t1rxnhRrF});
+    }
+    lFbYdl(1);
+    u6_UZSnQ(1);
+    for (int Y_xiOTPIsIt = 1; Y_xiOTPIsIt <= hbDL2G; Y_xiOTPIsIt++) printf("%lld\n", rGT0eQ[Y_xiOTPIsIt]);
+    return 0;
 }
