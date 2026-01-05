c7304b1ddd0577426675bed6f50680ec0a70ec48
Cyril-44 <zsy163mail1@163.com>
2026-01-06 07:24:01 +0800

    11445

--- !Luogu/P5586.cpp
+++ !Luogu/P5586.cpp
@@ -6,175 +6,67 @@
 #include <random>
 #include <type_traits>
 #include <cassert>
-constexpr int N = 300005, MOD = (int)1e9 + 7;
+constexpr int N = 300005;
+constexpr unsigned MOD = (unsigned)1e9 + 7;
 int rt[N];
 enum ProtoType1 { ADD, COV, FLP };
 enum ProtoType2 { COPY, SWAP };
 std::mt19937 rng(std::random_device{}());
 #define Inline __attribute__((always_inline)) inline
 class FastIS{static constexpr size_t h=1<<20;char o[h];char*t,*v;FILE*K;public:FastIS(FILE*B=stdin):t(nullptr),v(nullptr),K(B){}~FastIS(){fclose(K);}Inline int get(){return(t==v)&&(v=(t=o)+fread(o,1,h,K),t==v)?EOF:*t++;}template<typename j>typename std::enable_if<std::is_unsigned<j>::value&&std::is_integral<j>::value,FastIS&>::type inline operator>>(j&d){int a=get();while(~a&&(a<'0'||a>'9'))a=get();for(d=0;a>='0'&&a<='9';a=get())d=(d<<3)+(d<<1)+(a^'0');return*this;}template<typename j>typename std::enable_if<std::is_signed<j>::value&&std::is_integral<j>::value&&!std::is_same<j,bool>::value&&!std::is_same<j,char>::value,FastIS&>::type inline operator>>(j&d){int a=get();bool L=false;while(~a&&(a<'0'||a>'9')&&(a^'-'))a=get();if(a=='-')a=get(),L=true;for(d=0;a>='0'&&a<='9';a=get())d=(d<<3)+(d<<1)+(a^'0');d=L?-d:d;return*this;}template<typename j>typename std::enable_if<std::is_floating_point<j>::value,FastIS&>::type inline operator>>(j&d){int a=get();bool L=false;while(~a&&(a<'0'||a>'9')&&(a^'-'))a=get();if(a=='-')a=get(),L=true;using G=typename std::conditional<std::is_same<j,float>::value,std::uint32_t,std::uint64_t>::type;G I;for(I=0;a>='0'&&a<='9';a=get())I=(I<<3)+(I<<1)+(a^'0');d=I;if(a=='.'){G w=1;for(a=get();a>='0'&&a<='9';a=get())d+=(a^'0')*(1./(w*=10));}if(L)d=-d;return*this;}inline FastIS&operator>>(bool&U){int a=get();while(~a&&a!='0'&&a!='1')a=get();U=static_cast<bool>(a&1);return*this;}inline FastIS&operator>>(char&n){int a=get();while(a==' '||a=='\r'||a=='\n')a=get();n=a;return*this;}inline FastIS&operator>>(char*n){int a=get();while(~a&&(a==' '||a=='\r'||a=='\n'))a=get();while(~a&&(a!=' '&&a!='\r'&&a!='\n'))*n++=a,a=get();return*this;}}fin;class FastOS{static constexpr size_t h=1<<20;static constexpr double R=1e-9;char o[h],*O;const char*y;FILE*q;int l;public:FastOS(FILE*B=stdout):O(o),y(o+h),q(B),l(-1){setvbuf(q,nullptr,_IONBF,0);}~FastOS(){flush();fclose(q);}FastOS&flush(){fwrite(o,O-o,1,q);return*this;}FastOS&put(int H){*O++=H;if(O==y)fwrite(o,h,1,q),O=o;return*this;}FastOS&setprecision(const int&S){l=S;return*this;}template<typename j>typename std::enable_if<std::is_unsigned<j>::value&&std::is_integral<j>::value,FastOS&>::type inline operator<<(j d){static int n[32];n[*n=1]=d%10;while(d/=10)n[++(*n)]=d%10;while(*n)put(n[(*n)--]|'0');return*this;}template<typename j>typename std::enable_if<std::is_signed<j>::value&&std::is_integral<j>::value&&!std::is_same<j,char>::value,FastOS&>::type inline operator<<(const j&d){if(d<0)put('-')<<(typename std::make_unsigned<j>::type)(-d);else*this<<(typename std::make_unsigned<j>::type)(d);return *this;}template<typename j>typename std::enable_if<std::is_floating_point<j>::value,FastOS&>::type inline operator<<(j d){if(d<0)put('-'),d=-d;using G=typename std::conditional<std::is_same<j,float>::value,std::uint32_t,std::uint64_t>::type;G z=static_cast<G>(d);j O=d-z;*this<<z;if(l>0||l<0&&O>R){put('.');for(int J=0;l>0&&J<l||l<0&&O>R;J++){int N=static_cast<int>(O*=10);put(N|'0');O-=N;}}return*this;}FastOS&operator<<(const char&H){put(H);return*this;}}fout;
-namespace FHQ_Treap {
+namespace WBLT {
 #define U tr[u]
 #define UL tr[U.ls]
 #define UR tr[U.rs]
-inline void Add(int &x, int y) { if ((x += y) >= MOD) x -= MOD; }
+inline void plus(int &x, int y) { if ((x += y) >= MOD) x -= MOD; }
+inline void plus(int &x, int y, int z) { plus(x, y); plus(x, z); }
+enum SonType {Ls, Rs};
 struct Node {
-    decltype(rng()) prio;
-    int ls, rs, val, sum, sz, cov, add, refCnt;
-    bool rev;
-    inline Node(int x = 0) : prio(rng()), ls(), rs(), val(x), sum(x), sz(1), cov(-1), add(), refCnt(1), rev() {}
-    inline void cover(int x) { if (~x) add = 0, val = cov = x, sum = 1ull * x * sz % MOD; }
-    inline void plus(int x) { if (x) Add(add, x), Add(val, x), Add(sum, 1ull * x * sz % MOD); }
-    inline void flip() { std::swap(ls, rs); rev ^= 1; }
-} tr[N << 3];
-using Fn_t1 = void(*) (int, int);
-using Fn_t2 = void(*) (int&, int&);
-
-namespace Data {
-    int bin[N << 3], bintop = 0, tot = 0;
-    template<typename... Args>
-    inline int alloc(Args... args) {
-        if (bintop) { tr[bin[bintop]] = Node(args...); return bin[bintop--]; }
-        tr[++tot] = Node(args...); return tot;
+    int ls, rs;
+    int sz;
+    int dat, sum;
+    Node (int v = 0) : dat(v), sum(v) {}
+    struct Tag {
+        int cov, add; bool rev;
+        inline Tag() : cov(-1), add(), rev() {}
+    } tag;
+    inline void pull(const Tag &tp) {
+        if (~tp.cov) { dat = tag.cov = tp.cov; sum = (uint64_t)tp.cov * sz % MOD; }
+        if (!tp.add) { plus(dat, tp.add); plus(tag.add, tp.add); plus(sum, (uint64_t)tp.add * sz % MOD); }
+        if (tp.rev)  { std::swap(ls, rs); tag.rev ^= 1; }
     }
-    inline void delRef(int u) {
-        if (u && --U.refCnt == 0)
-            delRef(U.ls), delRef(U.rs), bin[++bintop] = u;
-    }
-}
-using Data::alloc; using Data::delRef;
-
-inline void addRef(int u) { u && ++U.refCnt; }
-inline void clone(int &u) {
-    if (U.refCnt == 1) return;
-    int v = alloc(U);
-    --U.refCnt; tr[v].refCnt = 1;
-    addRef(U.ls), addRef(U.rs);
-    u = v;
-}
-
-inline void pushup(int u) {
-    U.sz = UL.sz + 1 + UR.sz;
-    Add(U.sum = U.val, UL.sum), Add(U.sum, UR.sum);
-}
+} tr[N << 4];
+inline void pushup(int u) { U.sz = UL.sz + 1 + UR.sz; plus(U.sum = U.dat, UL.sum, UR.sum); }
 inline void pushdown(int u) {
-    if (U.ls) { clone(U.ls); if (U.rev) UL.flip(); UL.cover(U.cov), UL.plus(U.add); }
-    if (U.rs) { clone(U.rs); if (U.rev) UR.flip(); UR.cover(U.cov), UR.plus(U.add); }
-    U.cov = -1, U.add = 0, U.rev = false;
+    if (U.ls) UL.pull(U.tag);
+    if (U.rs) UR.pull(U.tag);
+    U.tag = Node::Tag();
 }
+inline bool isTooHeavy(int u) {
+    if (!U.ls)
+    if (std::min(tr[u]))
+}
+namespace Rotate {
+    inline void rotate() {
 
-int rt;
-void debug_all(int u = rt, int dep = 0) {
-    if (!u) return;
-    pushdown(u);
-    debug_all(U.ls, dep + 2);
-    printf("#%02d %2d: ", u, U.val);
-    for (int i = dep; i--; putchar(' '));
-    printf("sum=%d, sz=%d, refs=%d, prio_%lu, ls_%02d rs_%02d\n", U.sum, U.sz, U.refCnt, U.prio, U.ls, U.rs);
-    debug_all(U.rs, dep + 2);
-}
-void* _fhq_treap_initialization = []() {
-    tr[0].sz = 0;
-    int l = alloc(), r = alloc();
-    if (tr[l].prio < tr[r].prio) tr[rt = l].rs = r;
-    else tr[rt = r].ls = l;
-    pushup(rt);
-    return nullptr;
-}();
-std::pair<int,int> splitByOrd(int u, int ord) {
-    if (!u) return {0,0};
-    clone(u); pushdown(u);
-    if (UL.sz + 1 == ord) {
-        auto rs = U.rs;
-        U.rs = 0; pushup(u);
-        return {u, rs};
     }
-    if (UL.sz < ord) {
-        auto [ls, rs] = splitByOrd(U.rs, ord - UL.sz - 1);
-        U.rs = ls; pushup(u);
-        return {u, rs};
+    inline void merge() {
+
+    }z
+    inline void balance() {
+
     }
-    auto [ls, rs] = splitByOrd(U.ls, ord);
-    U.ls = rs; pushup(u);
-    return {ls, u};
 }
-int merge(int l, int r) {
-    if (!l || !r) return l | r;
-    clone(l); pushdown(l);
-    clone(r); pushdown(r);
-    if (1ull * tr[l].sz * rng() < 1ull * tr[r].sz * rng()) {
-        tr[l].rs = merge(tr[l].rs, r);
-        return pushup(l), l;
+namespace Non_Rotate {
+    inline void merge() {
+
     }
-    tr[r].ls = merge(l, tr[r].ls);
-    return pushup(r), r;
 }
-Fn_t1 oneRangeFuncs[3]{
-    [](int u, int x){ U.plus(x); },
-    [](int u, int x){ U.cover(x); },
-    [](int u, int){ U.flip(); }
-};
-Fn_t2 twoRangeFuncs[2]{
-    [](int &dest, int &src){ ++tr[src].refCnt; delRef(dest); clone(dest = src); },
-    [](int &dest, int &src){ std::swap(dest, src); }
-};
-namespace Utils {
-inline void push_back(int x) {
-    auto [ls, rs] = splitByOrd(rt, tr[rt].sz - 1);
-    rt = merge(merge(ls, alloc(x)), rs);
-}
-inline void modify(int destL, int destR, int srcL, int srcR, ProtoType2 tp) {
-    ++destR, ++srcR;
-    std::pair<int,int> sorted[2]{std::pair<int,int>{destL, destR}, std::pair<int,int>{srcL, srcR}};
-    if (sorted[1] < sorted[0]) std::swap(sorted[0], sorted[1]);
-    auto [tmp1, rs] = splitByOrd(rt, sorted[1].second);
-    auto [tmp2, u2] = splitByOrd(tmp1, sorted[1].first);
-    auto [tmp3, mid] = splitByOrd(tmp2, sorted[0].second);
-    auto [ls, u1] = splitByOrd(tmp3, sorted[0].first);
-    if (std::make_pair(destL, destR) != sorted[0]) twoRangeFuncs[tp](u2, u1);
-    else twoRangeFuncs[tp](u1, u2);
-#ifdef DEBUG
-    printf("==== ls ====\n"), debug_all(ls);
-    printf("==== u1 ====\n"), debug_all(u1);
-    printf("==== md ====\n"), debug_all(mid);
-    printf("==== u2 ====\n"), debug_all(u2);
-    printf("==== rs ====\n"), debug_all(rs);
-    printf("==== [%d, %d] ==== [%d, %d] ====\n", sorted[0].first, sorted[0].second, sorted[1].first, sorted[1].second);
-    fflush(stdout);
-#endif
-    rt = merge(merge(merge(ls, u1), mid), merge(u2, rs));
-}
-inline void modify(int l, int r, ProtoType1 tp, int x = 0) {
-    auto [mid, rs] = splitByOrd(rt, ++r);
-    auto [ls, u] = splitByOrd(mid, l);
-#ifdef DEBUG
-    printf("====== ls ======\n"), debug_all(ls);
-    printf("====== u =======\n"), debug_all(u);
-    printf("====== rs ======\n"), debug_all(rs);
-#endif
-    oneRangeFuncs[tp](u, x);
-    rt = merge(merge(ls, u), rs);
-}
-inline int querySum(int l, int r) {
-    auto [mid, rs] = splitByOrd(rt, ++r);
-    auto [ls, u] = splitByOrd(mid, l);
-    auto res = U.sum;
-    rt = merge(merge(ls, u), rs);
-    return res;
-}
-void extract(std::vector<int>& arr, int u = rt) {
-    if (!u) return;
-    pushdown(u);
-    extract(arr, U.ls);
-    arr.push_back(U.val);
-    extract(arr, U.rs);
+inline void merge() {
+    
 }
 }
-#undef U
-#undef UL
-#undef UR
-}
-using namespace FHQ_Treap::Utils;
+using namespace WBLT::Utils;
 int main() {
     // freopen("P5350.out", "w", stdout);
     int n, m;
