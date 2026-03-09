237441b0d759dcd250dab2c78ca1db1a51f2bd76
Hardsoft <Hardsoft.Cyril@outlook.com>
2026-03-09 21:02:12 +0800

    sale

--- !Luogu/P14636.cpp
+++ !Luogu/P14636.cpp
@@ -1,34 +1,39 @@
 #include <stdio.h>
 #include <algorithm>
 constexpr int N = 5005, MOD = 998244353;
-int a[N], pw2[N], C[N][N];
+int pw2[N], C[N][N];
+struct Node {
+    int price, id;
+    explicit operator int() const { return price; }
+} a[N];
+inline bool operator< (const Node &x, const Node &y) {
+    return (int)x > (int)y || int(x) == int(y) && x.id < y.id;
+}
 inline void add(int &x, int y) { if ((x += y) >= MOD) x -= MOD; }
 int main() {
     pw2[0] = 1;
-    for (int i = 1; i ^ N; i++) add(pw2[i] = pw2[i-1], pw2[i-1]);
-    for (int i = 0; i ^ N; i++) {
+    int Tid, T, n = 5000, m;
+    for (int i = 1; i <= n; i++) add(pw2[i] = pw2[i-1], pw2[i-1]);
+    for (int i = 0; i <= n; i++) {
         C[i][0] = C[i][i] = 1;
         for (int j = 1; j < i; j++)
             add(C[i][j] = C[i-1][j-1], C[i-1][j]);
     }
-    int Tid, T, n, m;
     scanf("%d%d", &Tid, &T);
     while (T--) {
         scanf("%d%d", &n, &m);
         for (int i = 1; i <= n; i++)
-            scanf("%d", &a[i]);
-        std::sort(a, a+1 + n);
-        int ans = 0;
-        for (int j = 0; j < n; j++)
-            for (int i = j+1; i <= n; i++) {
-                // 找方案使得 存在 k ，ak/2 性价比紧跟在 ai 之后，ak/2 ~ aj 之间的所有 wi=2，且 ai+aj < ak
-                int st = std::upper_bound(a+1, a+1+n, a[i]+a[j]) - a,
-                    ed = std::lower_bound(a+1, a+1+n, 2*a[i]) - a;
-                for (int k = st; k < ed; k++) {
-                    // k w=2, (k, ed) w 都是 1, [j, i] w 也都是 1
-                    int used = ed - k;
-                    if (n - i >= used && m >= (2 - !j))
-                        add(ans, 1ull * C[n - i - used][m - (2 - !j)] * (j>0?pw2[j-1]:1) % MOD);
+            scanf("%d", &a[i].price), a[i].id = i;
+        std::sort(a+1, a+1+n);
+        int ans = 0; // 不合法的方案数
+        for (int i = 1; i < n; i++)
+            for (int j = i+1; j <= n; j++) {
+                int L = std::upper_bound(a+1, a+1+n, 2 * int(a[i]), [](int x, const Node&y){return x > (int)y;}) - a;
+                int R = std::lower_bound(a+1, a+1+n, int(a[i]) + int(a[j]), [](const Node&x, int y){return int(x) > y;}) - a;
+                for (int k = L; k < R; k++) { // k 就是当前选定的 ai+aj < ak
+                    int force1Cnt = k - L;
+                    // 现在要选 k(2), ...(cnt个1)..., i(1), j(1)，前面的剩余的 (i - 2 - cnt) 个随便选
+                    if (i - 2 - force1Cnt >= 0 && m - i - 2 >= 0) add(ans, 1ull* C[i - 2 - force1Cnt][m - i - 2] * pw2[n - j]);
                 }
             }
         printf("%d\n", (pw2[n] - ans + MOD) % MOD);
