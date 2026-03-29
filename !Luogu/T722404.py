967946fac8979c9357941396df2947563a9fa85f
Cyril-44 <zsy163mail1@163.com>
2026-03-29 22:42:35 +0800

    Escape's Metabox

---
+++ !Luogu/T722404.py
@@ -0,0 +1,44 @@
+print(
+    "ddddd" #1
+    "wddsssaa" #2 d后两个箱子
+    "aaaaa" #3
+    "dddd" #4
+    "dsddasa" #5 推 箱子1
+    "aaswwwa" #5 推 箱子2
+    "ssasawaaaaa" #5 回到 4 到了 6
+    "asaasaawwd" #6 尝试到 7 推 7 到右
+    "assddwwww" #6 再将 7 推到最上面然后进入 7
+    "wdwwwasssss" #7 尝试将 8 推回去
+    "sswaassdd" #6 尝试进入 8
+    "wdwdss" #8 将 8 推走
+    "waasa" #8 在反向进入 8
+    "dddsdwww" #8 推到最上面
+    "ssaaawwaaa" #8 在反向进入 8 然后通往 9
+    "asssaaww" #9 尝试进入 10
+    "waaaa" #10 进入 11
+    "aaaaawwdddddd" #11 再进入 10
+    "dddwwaaaa" #10 再进入 11
+    "aaaww" #11 进入 9
+    "aw" #9 进入 12
+    "waaww" #12 进入 13
+    "wwwwwdddd" #13 进入 14
+    "dddww" #14 进入 12
+    "dw" #12 进入 13
+    "wwwwwdddd" #13 进入 14
+    "ddddssss" #14 进入 12
+    "sss" #12 进入 15
+    "ssass" #15 进入 16（推了1个箱子）
+    "saassaa" #16 再进入 15
+    "dassddww" #15 将又一个箱子推到 16 里面并且进入 16
+    "wddwwwwdd" #16 再进入 15
+    "dsdddd" #15 进入 17
+    "sssdddddwddsaaaaaaaawwd" 
+    "sssdddddwddsaaaaaaawwssaaaasawwwwwwwsdd"
+    "sssdddddwddsaaaaaaawwssaaaasawwwwwwwwwwawddddaaasssssdd"
+    "sssdddddwddsaaaaaaawwssaaaasawwwwwwwwwwawdddddddwdsssssasdd"
+    "sssdddddwddsaaaaaaawwssaaaasawwwwwwwwwwawdddddddwdsssssasdddddsdwwwwsssaaaa"
+    "sssdddddwddsaaaaaaawwssaaaasawwwwwwwwwwawdddddddwdsssssasdddddsdwwwwwwwawddddddddwwddww"  #20
+    "wwwaaaassd" #21
+    # "dwwdddd"
+)
+# 13, 14, 16, 18, 19, 22 TLE
