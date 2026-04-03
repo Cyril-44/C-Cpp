6f8206ab7ca8c84c32fb997fe713d66ac9551a6b
Hardsoft <Hardsoft.Cyril@outlook.com>
2026-04-03 21:11:29 +0800

    fk

--- !OtherOJ/国创杯小学组2026/string/std/std2.cpp
+++ !OtherOJ/国创杯小学组2026/string/std/std2.cpp
@@ -2,10 +2,10 @@
 #include <cstring>
 #include <algorithm>
 constexpr int N = 5000005;
-int f[N], sta[N], a[N][3], g[N];
+int f[N], a[N][3], g[N];
 char s[N];
 int main() {
-    int T, n, m, top;
+    int T, n, m;
     scanf("%d", &T);
     while (T--) {
         scanf("%s", s+1);
@@ -17,12 +17,18 @@ int main() {
             else a[++m][0] = s[i], a[m][1] = 1, a[m][2] = i;
         }
 
+        memset(g, 0, sizeof(int) * (n+1));
         for (int i = 1, j, k; i <= m; i++) {
-            j = a[i][2], k = a[i][2] + a[i][1] - 1;
-            
+            j = a[i][2] - 1, k = a[i][2] + a[i][1];
+            while (j > 0 && k <= n && s[j] == s[k] && s[j] <= s[j+1]) g[k++] = j--;
         }
-        top = 0;
-        a[m+1][0] = 114514;
+        for (int i = 1; i <= m; i++) {
+            int mnf = f[a[i][2]-1];
+            for (int j = a[i][2]; j - a[i][2] < a[i][1]; j++) {
+                f[j] = mnf + 1;
+                if (g[j]) mnf = std::min(mnf, f[j] = std::min(f[j], f[g[j] - 1] + 1));
+            }
+        }
         printf("%d\n", f[n]);
     }
     return 0;
