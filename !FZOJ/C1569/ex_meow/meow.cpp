10292f8573b7b2b1ac460e4875552c573959847b
Hardsoft <Hardsoft.Cyril@outlook.com>
2026-04-19 17:35:22 +0800

    meow

--- !FZOJ/C1569/ex_meow/meow.cpp
+++ !FZOJ/C1569/ex_meow/meow.cpp
@@ -1,61 +1,51 @@
 #include <bits/stdc++.h>
-constexpr int N = 1005, MOD = (int)1e9 + 7;
-int v[N], now[N], fact[N];
-bool vv[20][20];
-inline int qpow(int b, int n) {
-    int res = 1;
-    while (n) {
-        if (n & 1) res = 1ull * res * b % MOD;
-        b = 1ull * b * b % MOD;
-        n >>= 1;
-    }
-    return res;
-}
+constexpr int N = 505, MOD = (int)1e9 + 7;
+int v[N];
+
+struct ModInt {
+    inline ModInt(int x = 0) : val(x) {}
+    template<class T> inline explicit operator T() const { return static_cast<T>(val); }
+    inline ModInt operator+(ModInt o) const { return ModInt(*this) += o; }
+    inline ModInt operator-(ModInt o) const { return ModInt(*this) -= o; }
+    inline ModInt operator*(ModInt o) const { return ModInt(1ull * val * o.val % MOD); }
+    inline ModInt& operator+=(ModInt o) { if ((val += o.val) >= MOD) val -= MOD; return *this; }
+    inline ModInt& operator-=(ModInt o) { if ((val -= o.val) < 0) val += MOD; return *this; }
+    inline ModInt& operator*=(ModInt o) { return *this = *this * o; }
+    private: int val;
+} f[2][N][N];
+// f[i][j][k] 表示已经考虑了 v[1..i]，可以往下接的点(v[i]>i)总共有 j 个，前 i 个中有 k 个
+bool flg[N]; // 要记录一下有哪些点无法取前面的父亲
 int main() {
     int n;
     scanf("%d", &n);
-    fact[0] = 1;
-    for (int i = 1; i <= n; i++) fact[i] = 1ull * fact[i-1] * i % MOD;
-    bool TestFlagA = true, TestFlagB = true;
+    for (int i = 1; i <= n; i++) scanf("%d", &v[i]);
     for (int i = 1; i <= n; i++) {
-        scanf("%d", &v[i]);
-        if (v[i] <= i) TestFlagA = false;
-        if (v[i] != 0) TestFlagB = false;
+        if (v[i] && v[v[i]] && v[v[i]] <= v[i])
+            { puts("0"); return 0; }
+        flg[v[i]] = true;
     }
-    if (TestFlagA) return puts("1"), 0;
-    // else if (TestFlagB) return printf("%d\n", qpow(n, n)), 0;
-    else {
-        std::vector<int> vf(n), vb(n);
-        std::iota(vf.begin(), vf.end(), 1);
-        std::iota(vb.begin(), vb.end(), 1);
-        std::set<std::vector<int>> st;
-        unsigned long long ans = 0;
-        do {
-            do {
-                for (int i = 1; i <= n; i++) {
-                    int pl, pr;
-                    for (int j = 0; j < n; j++) if (vf[j] == i) pl = j;
-                    for (int j = 0; j < n; j++) if (vb[j] == i) pr = j;
-                    std::vector<bool> vis(n+2);
-                    for (int j = pl; j < n; j++) vis[vf[j]] = 1;
-                    for (int j = 0; j <= pr; j++) vis[vb[j]] = 1;
-                    for (now[i] = 1; vis[now[i]]; ++now[i]);
+    for (int i = 0; i <= n; i++) f[0][i][0] = 1;
+    for (int i = 1; i <= n; i++) {
+        memset(f[i&1], 0, sizeof f >> 1);
+        for (int j = 0; j <= n; j++)
+            for (int k = 0; k <= j; k++) {
+                if (v[i]) {
+                    if (v[i] <= i) f[i&1][j][k] = f[i&1^1][j][k];
+                    else if (k > 0) f[i&1][j][k] = f[i&1^1][j][k-1];
+                } else {
+                    if (!flg[i]) f[i&1][j][k] = f[i&1^1][j][k] * k; // v[i] <= i
+                    if (k > 0) f[i&1][j][k] += f[i&1^1][j][k-1] * (j - k + 1); // v[i] > i
                 }
-                bool flg = true;
-                for (int i = 1; i <= n; i++)
-                    if (v[i] && v[i] != now[i])
-                        { flg = false; break; }
-                if (flg) {
-                    st.insert(std::vector<int>(now+1, now+1+n));
-                }
-            } while (std::next_permutation(vb.begin(), vb.end()));
-        } while (std::next_permutation(vf.begin(), vf.end()));
-        for (const auto &v : st) {
-            for (int i : v)
-                printf("%d ", i);
-            putchar('\n');
-        }
-        printf("%zu\n", st.size() % MOD);
+            }
+        /* for (int j = 0; j <= n; j++)
+            for (int k = 0; k <= n; k++)
+                printf("%d%c", int(f[i&1][j][k]), " \n"[k==n]); */
     }
+    ModInt ans(0);
+    for (int i = 0; i <= n; i++) {
+        ans += f[n&1][i][i];
+        // printf("%d\n", int(f[n&1][i][i]));
+    }
+    printf("%d\n", int(ans));
     return 0;
 }
