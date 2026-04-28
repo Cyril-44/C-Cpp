a1a21cc5078e4da2b858857399cf1c6b6b1b2aa7
Hardsoft <Hardsoft.Cyril@outlook.com>
2026-04-28 21:02:20 +0800

    jiba show

--- !FZOJ/C1570/ex_show/show.cpp
+++ !FZOJ/C1570/ex_show/show.cpp
@@ -2,15 +2,14 @@
 #include <cstdint>
 #include <cassert>
 #include <cstring>
+#include <random>
 #include <algorithm>
 #include <map>
-constexpr int N = 50005, M = 100005;
+constexpr int N = 1005;
 struct Point {
     int x, y, v;
     inline bool operator<(const Point &o) const { return x + y < o.x + o.y; }
 } pnts[N << 1];
-int64_t sum[M];
-int n;
 /*
 考虑只有 Pos_b < Pos_c 或者 Pos_d < Pos_a 的时候 p 才会有负贡献。
 设置 F[i][j] 表示当前已经选了前 i 个红色和前 j 个紫色的位置，产生的最小负贡献。
@@ -18,77 +17,151 @@ int n;
 发现 (b,c) 对应的点如果有贡献一定在最终路径的上方，(a,d) 则是下方。
     有题目保证 a<b 且 c<d，我们知道 (b,c) 在 (a,d) 的左上方，不存在选了 (b,c) 没选 (a,d) 的情况。
     所以我们直接设置 (a,d) 权值为 p，(b,c) 权值为 
-转移显然是 F[i][j] = min(F[i-1][j], F[i][j-1]) + A[i][j]，其中 A 是点的权值。
+换一种方式去理解这个 DP，考虑 j 是高度，那么有 F[i][j] = min(F[i-1][1]...F[i-1][j]) + Sum[i][j]，其中 Sum[i][j] 是 sum_{k=1}^j A[i][k]。
+那么我们只需要每个转移做一个 Prefix-Min，然后去加上前缀和即可。
+直接暴力 Prefix-Min，暴力加即可。
+可以使用线段树二分去模拟 Treap 的 lower_bound 分裂。
 */
-struct SegTr {
-    struct Node { int64_t min, cov, add; } tr[N << 2];
-    int P, L, R;
-    int64_t X;
-    inline void pushdown(int u) {
-        if (tr[u].cov) {
-            tr[u<<1].min = tr[u<<1|1].min =
-            tr[u<<1].cov = tr[u<<1|1].cov = tr[u].cov;
-            tr[u<<1].add = tr[u<<1|1].add = 0;
-            tr[u].cov = 0;
+
+struct Treap {
+    constexpr static int64_t COV_DFT = (1ll << 62) - 1;
+    std::mt19937 rng;
+    int totcnt;
+    Treap() : rng(std::random_device{}()), totcnt(), rt() {}
+    struct Node {
+        int ls, rs, cnt;
+        int64_t val, lmx, rmn, add, cov;
+        uint64_t weight;
+        inline void pull(int64_t p, int64_t c) {
+            if (c != COV_DFT) val = lmx = rmn = cov = c, add = 0;
+            if (p) val += p, lmx += p, rmn += p, add += p;
         }
-        if (tr[u].add) {
-            tr[u<<1].add += tr[u].add, tr[u<<1|1].add += tr[u].add;
-            tr[u<<1].min += tr[u].add, tr[u<<1|1].min += tr[u].add;
-            tr[u].add = 0;
-        }
-    }
+    } tr[N << 1];
+    int rt;
     inline void pushup(int u) {
-        tr[u].min = tr[u<<1].min + tr[u<<1|1].min;
+        tr[u].cnt = tr[tr[u].ls].cnt + 1 + tr[tr[u].rs].cnt;
+        tr[u].lmx = tr[u].ls ? tr[tr[u].ls].lmx : tr[u].val;
+        tr[u].rmn = tr[u].rs ? tr[tr[u].rs].rmn : tr[u].val;
     }
-    void add(int u, int l, int r) {
-        if (L <= l && r <= R) tr[u].min += X, tr[u].add += X;
-        else {
-            int mid = l + r >> 1;
-            pushdown(u);
-            if (L <= mid) add(u<<1, l, mid);
-            if (mid < R) add(u<<1|1, mid + 1, r);
-            pushup(u);
-        }
+    inline void pushdown(int u) {
+        if (tr[u].add == 0 && tr[u].cov == COV_DFT) return;
+        if (tr[u].ls) tr[tr[u].ls].pull(tr[u].add, tr[u].cov);
+        if (tr[u].rs) tr[tr[u].rs].pull(tr[u].add, tr[u].cov);
+        tr[u].add = 0, tr[u].cov = COV_DFT;
     }
-    void cov(int u, int l, int r) {
-        if (L <= l && r <= R) tr[u].min = X, tr[u].cov = X, tr[u].add = 0;
-        else {
-            int mid = l + r >> 1;
-            pushdown(u);
-            if (L <= mid) cov(u<<1, l, mid);
-            if (mid < R) cov(u<<1|1, mid + 1, r);
-            pushup(u);
-        }
+    inline int alloc() {
+        tr[++totcnt] = { 0,0, 1, 0ll,0ll,0ll,0ll, COV_DFT, rng() };
+        return totcnt;
     }
-    int64_t que(int u, int l, int r) {
-        if (l == r) return tr[u].min;
-        int mid = l + r >> 1;
+    inline void push_back() {
+        rt = merge(rt, alloc());
+    }
+    int merge(int u, int v) {
+        if (!u || !v) return u | v;
+        if (tr[u].weight < tr[v].weight)
+            return pushdown(u), tr[u].rs = merge(tr[u].rs, v), pushup(u), u;
+        return pushdown(v), tr[v].ls = merge(u, tr[v].ls), pushup(v), v;
+    }
+    std::pair<int,int> lower_bound(int u, int64_t w) { // Find the first position to insert w, and split into 2 parts
+        if (tr[u].lmx <= w) return {0, u};
+        if (tr[u].rmn > w) return {u, 0};
         pushdown(u);
-        if (P <= mid) return que(u<<1, l, mid);
-        return que(u<<1|1, mid + 1, r);
+        if (w > tr[u].val) {
+            auto ret = lower_bound(tr[u].ls, w);
+            tr[u].ls = ret.second;
+            pushup(u);
+            return {ret.first, u};
+        }
+        auto ret = lower_bound(tr[u].rs, w);
+        tr[u].rs = ret.first;
+        pushup(u);
+        return {u, ret.second};
     }
-    inline void update(int l, int r, int x, bool mode) {
-        L=l, R=r, X=x;
-        if (mode) cov(1, 1, n);
-        else add(1, 1, n);
+    std::pair<int,int> split(int u, int w) { // Split the tree to {lsize=w, r}
+        if (w == 0) return {0, u};
+        if (tr[u].cnt <= w) return {u, 0};
+        if (tr[u].cnt < w) assert(0);
+        pushdown(u);
+        if (tr[tr[u].ls].cnt >= w) {
+            auto ret = split(tr[u].ls, w);
+            tr[u].ls = ret.second;
+            pushup(u);
+            return {ret.first, u};
+        }
+        auto ret = split(tr[u].rs, w - tr[tr[u].ls].cnt - 1);
+        tr[u].rs = ret.first;
+        pushup(u);
+        return {u, ret.second};
     }
-    inline int64_t inquire(int p, int x) {
-        P=p, X=x;
-        return que(1, 1, n);
+    inline int64_t inquireLast(int u) { return tr[u].rmn; }
+    void debug(int u) {
+        if (!u) return;
+        pushdown(u);
+        assert(!tr[u].ls || tr[u].weight < tr[tr[u].ls].weight);
+        assert(!tr[u].rs || tr[u].weight < tr[tr[u].rs].weight);
+        debug(tr[u].ls);
+        printf(" %ld[%d] ", tr[u].val, tr[u].cnt);
+        debug(tr[u].rs);
     }
-};
-std::map<int,int> v[N];
-int main() {
-    int m, k;
+} f; // 当前这一列的 dp 数组
+std::map<int,int64_t> v[N];
+namespace BF {
+    int64_t f[25][25], s[25][25];
+    inline int64_t work(int n, int m) {
+        for (int i = 1; i <= n; i++) {
+            int64_t premn = 0;
+            for (int j = 1; j <= m; j++) {
+                s[i][j] = s[i][j-1] + v[i][j];
+                f[i][j] = (premn = std::min(premn, f[i-1][j]) + s[i][j]);
+            }
+        }
+        return f[n][m];
+    }
+}
+int main(int argc, char** argv) {
+    int n, m, k;
     scanf("%d%d%d", &n, &m, &k);
+    int64_t sum = 0;
     for (int i = 1; i <= k; i++) {
         int a, b, c, d, p;
         scanf("%d%d%d%d%d", &a, &b, &c, &d, &p);
         v[d][a] += p;
         v[b][c] -= p;
+        printf("(%d,%d)+=%d\n(%d,%d)-=%d\n", d,a,p,b,c,p);
+        sum += p;
     }
-    
+    if (argc > 1 && !strcmp(argv[1], "dbg")) return printf("%ld\n", sum - BF::work(n, m)), 0;
+    for (int i = 0; i <= m; i++) f.push_back();
+    f.debug(f.rt); putchar('\n');
     int64_t ans;
-    printf("%ld\n", ans);
+    for (int i = 1; i <= n; i++) {
+        std::vector<int> rts; rts.reserve(1 + v[i].size());
+        rts.push_back(f.rt);
+        int lastPos = 0;
+        for (auto j : v[i]) {
+            printf("([%d], %ld, sp%d)", j.first, j.second, j.first - lastPos);
+            auto ret = f.split(rts.back(), j.first - lastPos);
+            lastPos = j.first;
+            rts.pop_back();
+            rts.push_back(ret.first);
+            rts.push_back(ret.second);
+            f.tr[rts.back()].pull(j.second, Treap::COV_DFT);
+            f.debug(ret.first); printf("| ");
+        }
+        f.debug(rts.back()); putchar('\n');
+        if (i == n) ans = f.inquireLast(rts.back());
+        std::reverse(rts.begin(), rts.end());
+        while (rts.size() > 1) {
+            int u1 = rts.back(); rts.pop_back();
+            int u2 = rts.back(); rts.pop_back();
+            int64_t last = f.inquireLast(u1);
+            auto ret = f.lower_bound(u2, last);
+            f.tr[ret.first].pull(0, last);
+            rts.push_back(f.merge(u1, f.merge(ret.first, ret.second)));
+        }
+        f.rt = rts.front();
+        f.debug(f.rt); putchar('\n');
+    }
+    printf("%ld\n", sum - ans);
     return 0;
 }
