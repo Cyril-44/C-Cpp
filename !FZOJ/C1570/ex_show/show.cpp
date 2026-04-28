be4534713c3320c07e04a2d061d3fc12ee6e16eb
Cyril-44 <zsy163mail1@163.com>
2026-04-29 01:02:19 +0800

    sofc show

--- !FZOJ/C1570/ex_show/show.cpp
+++ !FZOJ/C1570/ex_show/show.cpp
@@ -5,7 +5,7 @@
 #include <random>
 #include <algorithm>
 #include <map>
-constexpr int N = 1005;
+constexpr int N = 1000005;
 struct Point {
     int x, y, v;
     inline bool operator<(const Point &o) const { return x + y < o.x + o.y; }
@@ -16,7 +16,7 @@ struct Point {
 经典 trick：考虑将这个状态用平面直角坐标系去刻画，发现实质上我们要弄一条 (0,0) 到 (n,m) 的路径。
 发现 (b,c) 对应的点如果有贡献一定在最终路径的上方，(a,d) 则是下方。
     有题目保证 a<b 且 c<d，我们知道 (b,c) 在 (a,d) 的左上方，不存在选了 (b,c) 没选 (a,d) 的情况。
-    所以我们直接设置 (a,d) 权值为 p，(b,c) 权值为 
+    所以我们直接设置 (a,d) 权值为 p，(b,c) 权值为 -p。
 换一种方式去理解这个 DP，考虑 j 是高度，那么有 F[i][j] = min(F[i-1][1]...F[i-1][j]) + Sum[i][j]，其中 Sum[i][j] 是 sum_{k=1}^j A[i][k]。
 那么我们只需要每个转移做一个 Prefix-Min，然后去加上前缀和即可。
 直接暴力 Prefix-Min，暴力加即可。
@@ -36,7 +36,7 @@ struct Treap {
             if (c != COV_DFT) val = lmx = rmn = cov = c, add = 0;
             if (p) val += p, lmx += p, rmn += p, add += p;
         }
-    } tr[N << 1];
+    } tr[N << 2];
     int rt;
     inline void pushup(int u) {
         tr[u].cnt = tr[tr[u].ls].cnt + 1 + tr[tr[u].rs].cnt;
@@ -79,8 +79,8 @@ struct Treap {
     }
     std::pair<int,int> split(int u, int w) { // Split the tree to {lsize=w, r}
         if (w == 0) return {0, u};
-        if (tr[u].cnt <= w) return {u, 0};
-        if (tr[u].cnt < w) assert(0);
+        if (tr[u].cnt == w) return {u, 0};
+        // if (tr[u].cnt < w) assert(0);
         pushdown(u);
         if (tr[tr[u].ls].cnt >= w) {
             auto ret = split(tr[u].ls, w);
@@ -112,9 +112,15 @@ namespace BF {
             int64_t premn = 0;
             for (int j = 1; j <= m; j++) {
                 s[i][j] = s[i][j-1] + v[i][j];
-                f[i][j] = (premn = std::min(premn, f[i-1][j]) + s[i][j]);
+                f[i][j] = (premn = std::min(premn, f[i-1][j])) + s[i][j];
             }
         }
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= m; j++)
+                printf("%ld%c", f[i][j], " \n"[j==m]);
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= m; j++)
+                printf("%ld%c", s[i][j], " \n"[j==m]);
         return f[n][m];
     }
 }
@@ -125,30 +131,30 @@ int main(int argc, char** argv) {
     for (int i = 1; i <= k; i++) {
         int a, b, c, d, p;
         scanf("%d%d%d%d%d", &a, &b, &c, &d, &p);
-        v[d][a] += p;
+        v[a][d] += p;
         v[b][c] -= p;
-        printf("(%d,%d)+=%d\n(%d,%d)-=%d\n", d,a,p,b,c,p);
+        // printf("(%d,%d)+=%d\n(%d,%d)-=%d\n", a,d,p,b,c,p);
         sum += p;
     }
-    if (argc > 1 && !strcmp(argv[1], "dbg")) return printf("%ld\n", sum - BF::work(n, m)), 0;
+    if (argc > 1 && !strcmp(argv[1], "dbg")) return printf("%ld\n", - BF::work(n, m)), 0;
     for (int i = 0; i <= m; i++) f.push_back();
-    f.debug(f.rt); putchar('\n');
+    // f.debug(f.rt); putchar('\n');
     int64_t ans;
     for (int i = 1; i <= n; i++) {
         std::vector<int> rts; rts.reserve(1 + v[i].size());
         rts.push_back(f.rt);
         int lastPos = 0;
         for (auto j : v[i]) {
-            printf("([%d], %ld, sp%d)", j.first, j.second, j.first - lastPos);
+            // printf("([%d], %ld, sp%d)", j.first, j.second, j.first - lastPos);
             auto ret = f.split(rts.back(), j.first - lastPos);
             lastPos = j.first;
             rts.pop_back();
             rts.push_back(ret.first);
             rts.push_back(ret.second);
             f.tr[rts.back()].pull(j.second, Treap::COV_DFT);
-            f.debug(ret.first); printf("| ");
+            // f.debug(ret.first); printf("| ");
         }
-        f.debug(rts.back()); putchar('\n');
+        // f.debug(rts.back()); putchar('\n');
         if (i == n) ans = f.inquireLast(rts.back());
         std::reverse(rts.begin(), rts.end());
         while (rts.size() > 1) {
@@ -160,8 +166,8 @@ int main(int argc, char** argv) {
             rts.push_back(f.merge(u1, f.merge(ret.first, ret.second)));
         }
         f.rt = rts.front();
-        f.debug(f.rt); putchar('\n');
+        // f.debug(f.rt); putchar('\n');
     }
-    printf("%ld\n", sum - ans);
+    printf("%ld\n", -ans);
     return 0;
 }
