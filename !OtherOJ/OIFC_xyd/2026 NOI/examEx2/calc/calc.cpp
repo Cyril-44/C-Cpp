cc7345361b3b084a333ee8e61aa9d4bdff9c6afb
Hardsoft <Hardsoft.Cyril@outlook.com>
2026-07-05 21:30:16 +0800

    ModInt Fix + calc done

--- !OtherOJ/OIFC_xyd/2026 NOI/examEx2/calc/calc.cpp
+++ !OtherOJ/OIFC_xyd/2026 NOI/examEx2/calc/calc.cpp
@@ -1,61 +1,53 @@
-#include <cstdio>
-#include <algorithm>
-#include <cstring>
-#include <vector>
+#include <bits/stdc++.h>
 constexpr int N = 5005, MOD = (int)1e9 + 7;
-struct Mint {
-    Mint(int v=0) : val(v) {}
-    Mint& operator+=(Mint o) { if ((val += o.val) >= MOD) val -= MOD; return *this; }
-    Mint& operator-=(Mint o) { if ((val -= o.val) < 0) val += MOD; return *this; }
-    Mint& operator*=(Mint o) { val = 1ull * val * o.val % MOD; return *this; }
-    friend Mint operator+(Mint a, Mint b) { return a += b; }
-    friend Mint operator-(Mint a, Mint b) { return a -= b; }
-    friend Mint operator*(Mint a, Mint b) { return a *= b; }
-private: int val;
-};
-inline Mint qpow(Mint b, int n) {
-    Mint res = 1;
-    while (n) {
-        if (n & 1) res *= b;
-        b *= b, n >>= 1;
-    }
-    return res;
-}
+#define For(i, s, t) for(int i=(s); i<=(t); i++)
+template<typename T>constexpr inline T modInv(T x,T y){assert(x!=0);T u=0,v=1,a=x,m=y,t;while(a!=0){t=m/a;std::swap(a,m-=t*a);std::swap(u-=t*v,v);}assert(m==1);return u;}template<class Mod,typename Mod::value_type Default=0>class MB{using Int=typename Mod::value_type;Int v;template<typename T>constexpr Int nrm(T x){if constexpr(std::is_unsigned_v<T>)return x<T(mod())?x:x%T(mod());else{Int res=-mod()<x&&x<mod()?x:x%mod();return(res<0?res+mod():res);}}public:static constexpr Int mod(){return Mod::value;}constexpr MB():v(Default){}template<typename T>constexpr MB(const T&r){v=nrm(r);}template<typename T>explicit constexpr operator T()const{return static_cast<T>(v);}constexpr Int operator()()const{return v;}constexpr MB&operator+=(const MB&r){if((v+=r.v)>=mod())v-=mod();return*this;}constexpr MB&operator-=(const MB&r){if((v-=r.v)<0)v+=mod();return*this;}constexpr MB&operator*=(const MB&r){if constexpr(std::is_same_v<Int,int>)v=nrm((uint64_t)v*r.v);else if constexpr(std::is_same_v<Int,int64_t>)v=nrm((unsigned __int128)v*r.v);else v=nrm(v*r.v);return*this;}constexpr MB&operator/=(const MB&r){return*this*=MB(modInv(r.v,mod()));}template<class T>constexpr MB&operator^=(T n){if(n<0)assert(v!=0),v=1/v,n=-n;MB tmp=*this;for(*this=1;n;n>>=1){if(n&1)*this*=tmp;tmp*=tmp;}return*this;}constexpr MB operator-()const{return MB(-v);}constexpr MB&operator++(){return*this+=1;}constexpr MB&operator--(){return*this-=1;}constexpr MB operator++(int){MB tmp=*this;++*this;return tmp;}constexpr MB operator--(int){MB tmp=*this;--*this;return tmp;}constexpr bool operator!()const{return!v;}constexpr friend MB operator+(MB l,const MB&r){return l+=r;}constexpr friend MB operator-(MB l,const MB&r){return l-=r;}constexpr friend MB operator*(MB l,const MB&r){return l*=r;}constexpr friend MB operator/(MB l,const MB&r){return l/=r;}constexpr friend MB operator==(MB l,const MB&r){return l.v==r.v;}constexpr friend MB operator!=(MB l,const MB&r){return l.v!=r.v;}template<class T>constexpr friend MB operator^(MB l,const T r){return l^=r;}template<typename IS>friend IS&operator>>(IS&is,MB&l){is>>l.v;l.v=l.nrm(l.v);return is;}template<typename OS>friend OS&operator<<(OS&os,const MB&r){return os<<r.v;}};
+using Mint = MB<std::integral_constant<std::decay_t<decltype(MOD)>, MOD>>;
+Mint inv[N];
 std::vector<int> g[N];
-Mint f[N][N], tf[N]; // f[i][j] 
+Mint f[N][N], tf[N], initf; // f[i][j] 表示以 i 为根的子树，保留若干边之后，当前子树大小为 j
+Mint initg; // f[i][0] 表示以 i 为根的子树，当前填的值一定确保满足要求
 int sz[N];
-void dfs(int u, int fa, bool flg) { // flg=0 表示父->子， flg=1 表示子->父
-    sz[u] = 1, f[u][1] = 1;
+void dfs(int u, int fa = 0, bool flg = false) { // flg=0 表示父->子， flg=1 表示子->父
+    sz[u] = 1, f[u][1] = initf, f[u][0] = initg;
     for (int v : g[u]) if (v != fa) {
-        memset(tf, 0, sizeof tf);
+        dfs(v, u, !flg);
+        Mint fvsum = std::accumulate(f[v]+1, f[v]+sz[v]+1, Mint(0));
+        tf[0] = f[u][0] * (f[v][0] + fvsum);
+        For(i, 1, sz[u]) tf[i] = f[u][i] * f[v][0];
+        memset(tf + sz[u]+1, 0, sz[v] << 2);
         if (!flg) {
-            for (int i = 1; i <= sz[u]; i++)
-                for (int j = 1; j <= sz[v]; j++)
+            For(i, 1, sz[u])
+                For(j, 1, sz[v])
                     tf[i+j] += f[u][i] * f[v][j];
-            memcpy()
-        } else {
-            for (int i = 1; i <= sz[u]; i++) {
-                tf[i] += f[u][i] ( std::);
-                for (int j = 1; j <= sz[v]; j++)
+        } else { // 容斥：子->父的拓扑序限制 = 无限制 - 父->子
+            For(i, 1, sz[u]) {
+                tf[i] += f[u][i] * fvsum;
+                For(j, 1, sz[v])
                     tf[i+j] -= f[u][i] * f[v][j];
             }
         }
-        sz[u] += sz[v];
+        memcpy(f[u], tf, (sz[u] += sz[v]) + 1 << 2);
     }
+    For(i, 1, sz[u]) f[u][i] *= inv[i]; // 最终的答案是拓扑序数量/总排列数量，而拓扑序数量=总排列数量/每个子树大小的阶乘积
 }
+/*
+套路性的将实数随机转化为 [1,n] 随机排列 
+*/
 int main() {
+    inv[1] = 1;
+    For(i, 2, 5000) inv[i] = -inv[MOD % i] * (MOD / i);
     int n, p, q;
     scanf("%d%d%d", &n, &p, &q);
+    Mint t = Mint(p) / q;
     for (int i = 1, u, v; i < n; i++) {
         scanf("%d%d", &u, &v);
         g[u].push_back(v);
         g[v].push_back(u);
     }
-    if (p < q) {
-        
-    } else {
-
-    }
+    if (p < q) initf = t, initg = 1 - t; // 区间：0 ~ t，选中 >t 的直接成功
+    else initf = 2 - t, initg = 0; // 区间：t-1 ~ 1，没有可以直接成功的
     dfs(1, 0);
+    printf("%d\n", std::accumulate(f[1], f[1] + n+1, Mint(0)));
     return 0;
 }
--- templates/ModInt.cpp
+++ templates/ModInt.cpp
@@ -14,175 +14,72 @@ constexpr inline T modInv(T x, T y) { // x * u ≡ 1 (mod y)
 
 template<class Mod, typename Mod::value_type Default = 0>
 requires std::integral<typename Mod::value_type>
-class MIB {
-    using Int = Mod::value_type;
+class ModBase {
+    using Int = typename Mod::value_type;
     Int value;
     template<typename T> constexpr Int normalize(T x) {
         if constexpr (std::is_unsigned_v<T>)
-            return static_cast<Int>(x < T(mod()) ? x : x % T(mod()));
+            return x < T(mod()) ? x : x % T(mod());
         else {
-            Int res = static_cast<Int>(-mod() < x && x < mod() ? x : x % mod());
+            Int res = -mod() < x && x < mod() ? x : x % mod();
             return (res < 0 ? res + mod() : res);
         }
     }
 public:
     static constexpr Int mod() { return Mod::value; }
-    constexpr MIB() : value(Default) {}
-    template<typename T> constexpr MIB(T rhs) { value = normalize(rhs); }
+    constexpr ModBase() : value(Default) {}
+    template<typename T> constexpr ModBase(const T& rv) { value = normalize(rv); }
     template<typename T> explicit constexpr operator T() const { return static_cast<T>(value); }
     constexpr Int operator()() const { return value; }
-    constexpr MIB& operator+=(MIB rhs) {
-        if ((value += rhs.value) >= mod()) value -= mod();
+    constexpr ModBase& operator+=(const ModBase& rv) {
+        if ((value += rv.value) >= mod()) value -= mod();
         return *this;
     }
-    constexpr MIB& operator-=(MIB rhs) {
-        if ((value -= rhs.value) < 0) value += mod();
+    constexpr ModBase& operator-=(const ModBase& rv) {
+        if ((value -= rv.value) < 0) value += mod();
         return *this;
     }
-    constexpr MIB& operator*=(MIB rhs) {
+    constexpr ModBase& operator*=(const ModBase& rv) {
         if constexpr (std::is_same_v<Int, int>)
-            value = normalize((uint64_t)value * rhs.value);
+            value = normalize((uint64_t)value * rv.value);
         else if constexpr (std::is_same_v<Int, int64_t>)
-            value = normalize((unsigned __int128)value * rhs.value);
+            value = normalize((unsigned __int128)value * rv.value);
         else 
-            value = normalize(value * rhs.value);
+            value = normalize(value * rv.value);
         return *this;
     }
-    constexpr MIB& operator/=(MIB rhs) { return *this *= MIB(modInv(rhs.value, mod())); }
-    template<std::integral T> constexpr MIB& operator^=(T rhs) {
-        if (rhs < 0) return *this = MIB(modInv(static_cast<Int>(1), (*this ^ (-rhs))()));
-        MIB tmp = *this;
-        for (*this = static_cast<Int>(1); rhs; rhs >>= 1) {
-            if (rhs & 1) *this *= tmp;
+    constexpr ModBase& operator/=(const ModBase& rv) { return *this *= ModBase(modInv(rv.value, mod())); }
+    template<std::integral T> constexpr ModBase& operator^=(T n) {
+        if (n < 0) assert(value != 0), value = 1 / value, n = -n;
+        ModBase tmp = *this;
+        for (*this = 1; n; n >>= 1) {
+            if (n & 1) *this *= tmp;
             tmp *= tmp;
         }
         return *this;
     }
-    constexpr MIB operator-() const { return MIB(-value); }
-    constexpr MIB& operator++() { return *this += 1; }
-    constexpr MIB& operator--() { return *this -= 1; }
-    constexpr MIB operator++(int) { MIB tmp = *this; ++*this; return tmp; }
-    constexpr MIB operator--(int) { MIB tmp = *this; --*this; return tmp; }
-    constexpr MIB operator+(MIB rhs) const { return MIB(*this) += rhs; }
-    constexpr MIB operator-(MIB rhs) const { return MIB(*this) -= rhs; }
-    constexpr MIB operator*(MIB rhs) const { return MIB(*this) *= rhs; }
-    constexpr MIB operator/(MIB rhs) const { return MIB(*this) /= rhs; }
-    template<std::integral T> constexpr MIB operator^(const T rhs) const { return MIB(*this) ^= rhs; }
-    constexpr bool operator==(MIB rhs) const { return value == rhs.value; }
-    constexpr bool operator!=(MIB rhs) const { return value != rhs.value; }
+    constexpr ModBase operator-() const { return ModBase(-value); }
+    constexpr ModBase& operator++() { return *this += 1; }
+    constexpr ModBase& operator--() { return *this -= 1; }
+    constexpr ModBase operator++(int) { ModBase tmp = *this; ++*this; return tmp; }
+    constexpr ModBase operator--(int) { ModBase tmp = *this; --*this; return tmp; }
     constexpr bool operator!() const { return !value; }
-    template<std::integral U> constexpr friend MIB operator+(U lhs, MIB rhs) { return MIB(lhs) + rhs; }
-    template<std::integral U> constexpr friend MIB operator-(U lhs, MIB rhs) { return MIB(lhs) - rhs; }
-    template<std::integral U> constexpr friend MIB operator*(U lhs, MIB rhs) { return MIB(lhs) * rhs; }
-    template<std::integral U> constexpr friend MIB operator/(U lhs, MIB rhs) { return MIB(lhs) / rhs; }
-    template<std::integral U> constexpr friend MIB operator==(U lhs, MIB rhs) { return MIB(lhs) == rhs; }
-    template<std::integral U> constexpr friend MIB operator!=(U lhs, MIB rhs) { return MIB(lhs) != rhs; }
-    template<std::integral U> constexpr friend MIB operator<=(U lhs, MIB rhs) { return MIB(lhs) <= rhs; }
-    template<std::integral U> constexpr friend MIB operator>=(U lhs, MIB rhs) { return MIB(lhs) >= rhs; }
-    template<std::integral U> constexpr friend MIB operator<(U lhs, MIB rhs) { return MIB(lhs) < rhs; }
-    template<std::integral U> constexpr friend MIB operator>(U lhs, MIB rhs) { return MIB(lhs) > rhs; }
-    template<typename IStream> friend IStream& operator>>(IStream& is, MIB& lhs) {
-        is >> lhs.value;
-        lhs.value = lhs.normalize(lhs.value);
+    constexpr friend ModBase operator+(ModBase lv, const ModBase& rv) { return lv += rv; }
+    constexpr friend ModBase operator-(ModBase lv, const ModBase& rv) { return lv -= rv; }
+    constexpr friend ModBase operator*(ModBase lv, const ModBase& rv) { return lv *= rv; }
+    constexpr friend ModBase operator/(ModBase lv, const ModBase& rv) { return lv /= rv; }
+    constexpr friend ModBase operator==(ModBase lv, const ModBase& rv) { return lv.value == rv.value; }
+    constexpr friend ModBase operator!=(ModBase lv, const ModBase& rv) { return lv.value != rv.value; }
+    template<std::integral T> constexpr friend ModBase operator^(ModBase lv, const T rv) { return lv ^= rv; }
+    template<typename IStream> friend IStream& operator>>(IStream& is, ModBase& lv) {
+        is >> lv.value;
+        lv.value = lv.normalize(lv.value);
         return is;
     }
-    template<typename OStream> friend OStream& operator<<(OStream& os, MIB rhs) { return os << rhs.value; }
-};
-template<class Mod, typename Mod::value_type Default = 0>
-requires requires(typename Mod::value_type a, typename Mod::value_type b) {
-    // 1. 基础算术操作 (你原本定义的)
-    { a + b } -> std::same_as<typename Mod::value_type>;
-    { a - b } -> std::same_as<typename Mod::value_type>;
-    { a * b } -> std::same_as<typename Mod::value_type>;
-    { a % b } -> std::same_as<typename Mod::value_type>;
-    // 2. 复合赋值操作 (+=, -=, *=, %=)
-    { a += b } -> std::same_as<typename Mod::value_type&>;
-    { a -= b } -> std::same_as<typename Mod::value_type&>;
-    { a *= b } -> std::same_as<typename Mod::value_type&>;
-    { a %= b } -> std::same_as<typename Mod::value_type&>;
-    // 3. 构造与赋值约束 (确保 Default = 0 以及常规初始化能通过)
-    requires std::is_default_constructible_v<typename Mod::value_type>;
-    requires std::is_copy_constructible_v<typename Mod::value_type>;
-    requires std::is_move_constructible_v<typename Mod::value_type>;
-    requires std::is_assignable_v<typename Mod::value_type&, int>; // 确保能从 0 构造/赋值
-    // 4. 比较操作 (==, !=)
-    requires std::equality_comparable<typename Mod::value_type>;
-} class MDB {
-    using Int = Mod::value_type;
-    Int value;
-    template<std::integral T> constexpr Int normalize(T x) {
-        if constexpr (std::is_unsigned_v<T>)
-            return static_cast<Int>(x < T(mod()) ? x : x % T(mod()));
-        else {
-            Int res = static_cast<Int>(-mod() < x && x < mod() ? x : x % mod());
-            return (res < 0 ? res + mod() : res);
-        }
-    }
-public:
-    static constexpr Int mod() { return Mod::value; }
-    constexpr MDB() : value(Default) {}
-    template<std::integral T> constexpr MDB(const T &rhs) { value = normalize(rhs); }
-    template<std::integral T> explicit constexpr operator T() const { return static_cast<T>(value); }
-    constexpr Int operator()() const { return value; }
-    constexpr MDB& operator+=(const MDB& rhs) {
-        if ((value += rhs.value) >= mod()) value -= mod();
-        return *this;
-    }
-    constexpr MDB& operator-=(const MDB& rhs) {
-        if ((value -= rhs.value) < 0) value += mod();
-        return *this;
-    }
-    constexpr MDB& operator*=(const MDB& rhs) {
-        if constexpr (std::is_same_v<Int, int>)
-            value = normalize((uint64_t)value * rhs.value);
-        else if constexpr (std::is_same_v<Int, int64_t>)
-            value = normalize((unsigned __int128)value * rhs.value);
-        else 
-            value = normalize(value * rhs.value);
-        return *this;
-    }
-    constexpr MDB& operator/=(const MDB& rhs) { return *this *= MDB(modInv(rhs.value, mod())); }
-    template<std::integral T> constexpr MDB& operator^=(T rhs) {
-        if (rhs < 0) return *this = MDB(modInv(static_cast<Int>(1), (*this ^ (-rhs))()));
-        MDB tmp = *this;
-        for (*this = static_cast<Int>(1); rhs; rhs >>= 1) {
-            if (rhs & 1) *this *= tmp;
-            tmp *= tmp;
-        }
-        return *this;
-    }
-    constexpr MDB operator-() const { return MDB(-value); }
-    constexpr MDB& operator++() { return *this += 1; }
-    constexpr MDB& operator--() { return *this -= 1; }
-    constexpr MDB operator++(int) { MDB tmp = *this; ++*this; return tmp; }
-    constexpr MDB operator--(int) { MDB tmp = *this; --*this; return tmp; }
-    constexpr MDB operator+(const MDB& rhs) const { return MDB(*this) += rhs; }
-    constexpr MDB operator-(const MDB& rhs) const { return MDB(*this) -= rhs; }
-    constexpr MDB operator*(const MDB& rhs) const { return MDB(*this) *= rhs; }
-    constexpr MDB operator/(const MDB& rhs) const { return MDB(*this) /= rhs; }
-    template<std::integral T> constexpr MDB operator^(const T rhs) const { return MDB(*this) ^= rhs; }
-    constexpr bool operator==(const MDB& rhs) const { return value == rhs.value; }
-    constexpr bool operator!=(const MDB& rhs) const { return value != rhs.value; }
-    constexpr bool operator!() const { return !value; }
-    template<typename U> constexpr friend MDB operator+(const U& lhs, const MDB& rhs) { return MDB(lhs) + rhs; }
-    template<typename U> constexpr friend MDB operator-(const U& lhs, const MDB& rhs) { return MDB(lhs) - rhs; }
-    template<typename U> constexpr friend MDB operator*(const U& lhs, const MDB& rhs) { return MDB(lhs) * rhs; }
-    template<typename U> constexpr friend MDB operator/(const U& lhs, const MDB& rhs) { return MDB(lhs) / rhs; }
-    template<typename U> constexpr friend MDB operator==(const U& lhs, const MDB& rhs) { return MDB(lhs) == rhs; }
-    template<typename U> constexpr friend MDB operator!=(const U& lhs, const MDB& rhs) { return MDB(lhs) != rhs; }
-    template<typename U> constexpr friend MDB operator<=(const U& lhs, const MDB& rhs) { return MDB(lhs) <= rhs; }
-    template<typename U> constexpr friend MDB operator>=(const U& lhs, const MDB& rhs) { return MDB(lhs) >= rhs; }
-    template<typename U> constexpr friend MDB operator<(const U& lhs, const MDB& rhs) { return MDB(lhs) < rhs; }
-    template<typename U> constexpr friend MDB operator>(const U& lhs, const MDB& rhs) { return MDB(lhs) > rhs; }
-    template<typename IStream> friend IStream& operator>>(IStream& is, MDB& lhs) {
-        is >> lhs.value;
-        lhs.value = lhs.normalize(lhs.value);
-        return is;
-    }
-    template<typename OStream> friend OStream& operator<<(OStream& os, const MDB& rhs) { return os << rhs.value; }
+    template<typename OStream> friend OStream& operator<<(OStream& os, const ModBase& rv) { return os << rv.value; }
 };
 constexpr auto MOD = (int)1e9 + 7;
-using Mint = MIB<std::integral_constant<std::decay_t<decltype(MOD)>, MOD>>;
+using Mint = ModBase<std::integral_constant<std::decay_t<decltype(MOD)>, MOD>>;
 // struct Dynamic_ModInt { using value_type = int; static value_type value; };
 // Dynamic_ModInt::value_type Dynamic_ModInt::value = 0;
 // Dynamic_ModInt::value_type &Mod = Dynamic_ModInt::value;
@@ -221,6 +118,7 @@ int main() {
     1 + Mint(1);
     Mint abc = 1;
     abc += 2;
+    sqrt(Mint(5));
     M9();
     return 0;
 }
