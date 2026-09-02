1b346c45195fab25fbfe876a593efe17661e787d
Cyril-44 <zsy163mail1@163.com>
2026-09-03 01:50:13 +0800

    fixed

--- !AtCoder/agc069_a.cpp
+++ !AtCoder/agc069_a.cpp
@@ -1,8 +1,8 @@
 #include <bits/extc++.h>
 constexpr int N = 18;
 __gnu_pbds::priority_queue<int> q[1<<N];
-int64_t f[N];
-int r[N];
+int64_t f[1<<N];
+int r[1<<N];
 int main() {
     int n;
     scanf("%d", &n);
@@ -10,12 +10,12 @@ int main() {
         scanf("%d%d", &l, &r[i]);
         q[i].push(l);
     }
-    for (int w = 0; w+1 < n; w++) {
+    for (int w = 0; w < n; w++) {
         for (int i = 0; i < (1<<n); i += 1<<w+1) {
             if (r[i] > r[i + (1<<w)]) std::swap(r[i], r[i + (1<<w)]);
             q[i].join(q[i + (1<<w)]);
             f[i] += f[i + (1<<w)];
-            if (q[i].top() > r[i]) f[i] += q[i].top() - r[i], q[i].push(r[i]);
+            if (q[i].top() > r[i]) f[i] += q[i].top() - r[i], q[i].pop(), q[i].push(r[i]);
             std::swap(r[i], r[i + (1<<w)]);
         }
     }
