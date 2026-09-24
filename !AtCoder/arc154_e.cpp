0cd652cb7415aa8446c79054698f56ef49391290
Hardsoft <Hardsoft.Cyril@outlook.com>
2026-09-24 21:31:20 +0800

    容斥

---
+++ !AtCoder/arc154_e.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+// #define LUOGU
+#if defined(ONLINE_JUDGE) && !defined(LUOGU)
+# pragma GCC optimize(2, 3, "inline", "unroll-loops", "fast-math", "inline-small-functions", "no-stack-protector", "delete-null-pointer-checks")
+# pragma GCC target("tune=native")
+#endif
+#define Inline __attribute__((always_inline)) inline
+#define For(i, s, t) for (int i = (s); i <= (t); ++i)
+#define Forv(i, s, t, ...) for (int i = (s), __VA_ARGS__; i <= (t); ++i)
+#define roF(i, t, s) for (int i = (t); i >= (s); --i)
+#define roFv(i, t, s, ...) for (int i = (t), __VA_ARGS__; i >= (s); --i)
+#define Rep(c) for (int tempFor_count = c; tempFor_count; --tempFor_count)
+#define Repv(c, ...) for (int tempFor_count = c, __VA_ARGS__; tempFor_count; --tempFor_count)
+#define YES return cout << "Yes\n", void()
+#define NO return cout << "No\n", void()
+#define YESNO(j) cout << ((j) ? "Yes\n" : "No\n")
+#define EXIT(s...) return (cout << s), void();
+using namespace std;using pii_t=pair<int,int>;using pll_t=pair<int64_t,int64_t>;using veci_t=vector<int>;using vecl_t=vector<int64_t>;Inline int Popcnt(int x){return __builtin_popcount((unsigned)x);}Inline int Popcnt(unsigned x){return __builtin_popcount(x);}Inline int Popcnt(int64_t x){return __builtin_popcountll((uint64_t)x);}Inline int Popcnt(uint64_t x){return __builtin_popcountll(x);}Inline int Log2(int x){return 31-__builtin_clz((unsigned)x|1);}Inline int Log2(unsigned x){return 31-__builtin_clz(x|1);}Inline int Log2(int64_t x){return 63-__builtin_clzll((uint64_t)x|1);}Inline int Log2(uint64_t x){return 63-__builtin_clzll(x|1);}
+
+// #define MULTI_TEST_CASES
+
+constexpr int N = 2e5;
+template<typename T>constexpr inline T modInv(T x,T y){assert(x!=0);T u=0,v=1,a=x,m=y,t;while(a!=0){t=m/a;std::swap(a,m-=t*a);std::swap(u-=t*v,v);}assert(m==1);return u;}template<class Mod,typename Mod::value_type Default=0>requires std::integral<typename Mod::value_type>class MB{using Int=Mod::value_type;Int v;template<typename T>constexpr Int nrm(T x){if constexpr(std::is_unsigned_v<T>)return x<T(mod())?x:x%T(mod());else{Int res=-mod()<x&&x<mod()?x:x%mod();return(res<0?res+mod():res);}}public:static constexpr Int mod(){return Mod::value;}constexpr MB():v(Default){}template<typename T>constexpr MB(const T&r){v=nrm(r);}template<typename T>explicit constexpr operator T()const{return static_cast<T>(v);}constexpr Int operator()()const{return v;}constexpr MB&operator+=(const MB&r){if((v+=r.v)>=mod())v-=mod();return*this;}constexpr MB&operator-=(const MB&r){if((v-=r.v)<0)v+=mod();return*this;}constexpr MB&operator*=(const MB&r){if constexpr(std::is_same_v<Int,int>)v=nrm((uint64_t)v*r.v);else if constexpr(std::is_same_v<Int,int64_t>)v=nrm((unsigned __int128)v*r.v);else v=nrm(v*r.v);return*this;}constexpr MB&operator/=(const MB&r){return*this*=MB(modInv(r.v,mod()));}template<std::integral T>constexpr MB&operator^=(T n){if(n<0)assert(v!=0),v=1/v,n=-n;MB tmp=*this;for(*this=1;n;n>>=1){if(n&1)*this*=tmp;tmp*=tmp;}return*this;}constexpr MB operator-()const{return MB(-v);}constexpr MB&operator++(){return*this+=1;}constexpr MB&operator--(){return*this-=1;}constexpr MB operator++(int){MB tmp=*this;++*this;return tmp;}constexpr MB operator--(int){MB tmp=*this;--*this;return tmp;}constexpr bool operator!()const{return!v;}constexpr friend MB operator+(MB l,const MB&r){return l+=r;}constexpr friend MB operator-(MB l,const MB&r){return l-=r;}constexpr friend MB operator*(MB l,const MB&r){return l*=r;}constexpr friend MB operator/(MB l,const MB&r){return l/=r;}constexpr friend bool operator==(MB l,const MB&r){return l.v==r.v;}constexpr friend bool operator!=(MB l,const MB&r){return l.v!=r.v;}template<std::integral T>constexpr friend MB operator^(MB l,const T r){return l^=r;}template<typename IS>friend IS&operator>>(IS&is,MB&l){is>>l.v;l.v=l.nrm(l.v);return is;}template<typename OS>friend OS&operator<<(OS&os,const MB&r){return os<<r.v;}};
+constexpr auto MOD = 998244353;
+using Mint = MB<std::integral_constant<std::decay_t<decltype(MOD)>, MOD>>;
+Mint sqrt(Mint x){return x^(-2);}struct Fact{Fact(const int n):aa(n+1,Mint(1)),ia(n+1),sz(n){aa[0]=1;for(int i=1;i<=n;i++)aa[i]=aa[i-1]*i;ia[n]=Mint(1)/aa[n];for(int i=n;i>=1;i--)ia[i-1]=ia[i]*i;}Mint C(int n,int m)const{if(n<0||m<0||n<m)[[unlikely]]return 0;if(n>sz)[[unlikely]]throw std::out_of_range("Expected n < "+std::to_string(sz)+", but found n = "+std::to_string(n)+".");return aa[n]*ia[m]*ia[n-m];}Mint A(int n,int m)const{if(n<0||m<0||n<m)[[unlikely]]return 0;if(n>sz)[[unlikely]]throw std::out_of_range("Expected n < "+std::to_string(sz)+", but found n = "+std::to_string(n)+".");return aa[n]*ia[n-m];}Mint F(int n)const{if(n<0)[[unlikely]]return 0;if(n>sz)[[unlikely]]throw std::out_of_range("Expected n < "+std::to_string(sz)+", but found n = "+std::to_string(n)+".");return aa[n];}private:std::vector<Mint>aa,ia;const int sz;};
+Fact F(N);
+int p[N+1];
+inline void solveSingleTestCase() {
+    int n, m;
+    cin >> n >> m;
+    For(i, 1, n) cin >> p[i];
+    Mint base = Mint(n * (n+1ll) / 2);
+    Mint dino = base ^ m, invdino = 1 / dino;
+    Mint mid = Mint(n+1) / 2;
+    Mint ans = 0;
+    for (int i = 1; i <= n; i++) {
+        Mint now = base - Mint(i) * Mint(n - i + 1);
+        Mint pprob = (now ^ m) * invdino, lprob = 1 - pprob;
+        ans += Mint(i) * i - p[i] * (i * pprob + mid * lprob);
+    }
+    cout << ans * dino;
+}
+int main() {
+    cin.tie(nullptr) -> sync_with_stdio(false);
+    int testCases = 1;
+#ifdef MULTI_TEST_CASES
+    cin >> testCases;
+#endif
+    while (testCases--) solveSingleTestCase();
+    return 0;
+}
